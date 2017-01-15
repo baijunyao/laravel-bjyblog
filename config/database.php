ccf205febda7f198f4c234b2a355d1b44ec2ecc9
baijunyao <baijunyao@baijunyao.com>
2017-01-15 17:51:41 +0800

    定义表前缀

--- config/database.php
+++ config/database.php
@@ -61,7 +61,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
+            'prefix' => 'bjy_',
             'strict' => true,
             'engine' => null,
         ],
