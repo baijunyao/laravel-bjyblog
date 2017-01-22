24c138b21e8cff10e272a8db95a0365098d39da7
baijunyao <baijunyao@baijunyao.com>
2017-01-22 10:54:11 +0800

    Base模型增加常用方法

--- app/Models/Base.php
+++ app/Models/Base.php
@@ -6,5 +6,110 @@ use Illuminate\Database\Eloquent\Model;
 
 class Base extends Model
 {
-    
+
+    /**
+     * 添加数据
+     *
+     * @param  array $data 需要添加的数据
+     * @return bool        是否成功
+     */
+    public function addData($data)
+    {
+
+        //添加数据
+        $result=$this
+            ->create($data)
+            ->id;
+        if ($result) {
+            session()->flash('alert-message','添加成功');
+            session()->flash('alert-class','alert-success');
+            return $result;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * 修改数据
+     *
+     * @param  array $map  where条件
+     * @param  array $data 需要修改的数据
+     * @return bool        是否成功
+     */
+    public function editData($map, $data)
+    {
+        //查找需要修改的数据
+        $model = $this->where($map)->first();
+        //修改
+        foreach ($data as $k => $v) {
+            $model->{$k} = $v;
+        }
+        $result = $model->save();
+        if ($result) {
+            session()->flash('alert-message','修改成功');
+            session()->flash('alert-class','alert-success');
+            return $result;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * 删除数据
+     *
+     * @param  array $map   where 条件数组形式
+     * @return bool         是否成功
+     */
+    public function deleteData($map)
+    {
+        //软删除
+        $result=$this
+            ->where($map)
+            ->delete();
+        if ($result) {
+            session()->flash('alert-message','操作成功');
+            session()->flash('alert-class','alert-success');
+            return $result;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * 排序
+     * @param  array $data 需要排序的数据
+     * @return bool        是否成功
+     */
+    public function orderData($data)
+    {
+        //如果存在_token字段；则删除
+        if (isset($data['_token'])) {
+            unset($data['_token']);
+        }
+        //判断是否有需要排序的字段
+        if (empty($data)) {
+            session()->flash('alert-message','没有需要排序的数据');
+            session()->flash('alert-class','alert-success');
+            return false;
+        }
+        //循环修改数据
+        foreach ($data as $k => $v){
+            $v = empty($v) ? null : $v;
+            $edit_data=[
+                'order_number'=>$v
+            ];
+            //修改数据
+            $result=$this
+                ->where('id',$k)
+                ->update($edit_data);
+        }
+        if ($result) {
+            session()->flash('alert-message','修改成功');
+            session()->flash('alert-class','alert-success');
+            return $result;
+        }else{
+            return false;
+        }
+    }
+
 }
