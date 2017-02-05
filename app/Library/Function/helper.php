601c64983e6dfd0f1cbc93eca6a050723ea29bab
baijunyao <baijunyao@baijunyao.com>
2017-02-05 17:22:18 +0800

    打印转成数组后的分页

--- app/Library/Function/helper.php
+++ app/Library/Function/helper.php
@@ -5,26 +5,26 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Auth;
 
 if ( !function_exists('p') ) {
-	//传递数据以易于阅读的样式格式化后输出
-	function p($data, $to_array = true)
-	{
-		// 定义样式
-		$str = '<pre style="display: block;padding: 9.5px;margin: 44px 0 0 0;font-size: 13px;line-height: 1.42857;color: #333;word-break: break-all;word-wrap: break-word;background-color: #F5F5F5;border: 1px solid #CCC;border-radius: 4px;">';
-		// 如果是boolean或者null直接显示文字；否则print
-		if (is_bool($data)) {
-			$show_data = $data ? 'true' : 'false';
-		} elseif (is_null($data)) {
-			$show_data = 'null';
-		} elseif (get_parent_class($data) === 'Illuminate\Support\Collection' && $to_array) {
-			$data_array = $data->toArray();
-			$show_data = '这是被转成数组的对象:<br>'.print_r($data_array, true);
-		} else {
-			$show_data = print_r($data, true);
-		}
-		$str .= $show_data;
-		$str .= '</pre>';
-		echo $str;
-	}
+    // 传递数据以易于阅读的样式格式化后输出
+    function p($data, $to_array = true)
+    {
+        // 定义样式
+        $str = '<pre style="display: block;padding: 9.5px;margin: 44px 0 0 0;font-size: 13px;line-height: 1.42857;color: #333;word-break: break-all;word-wrap: break-word;background-color: #F5F5F5;border: 1px solid #CCC;border-radius: 4px;">';
+        // 如果是 boolean 或者 null 直接显示文字；否则 print
+        if (is_bool($data)) {
+            $show_data = $data ? 'true' : 'false';
+        } elseif (is_null($data)) {
+            $show_data = 'null';
+        } elseif (in_array(get_parent_class($data), ['Illuminate\Support\Collection', 'Illuminate\Pagination\AbstractPaginator', 'App\Models\Base']) && $to_array) {
+            $data_array = $data->toArray();
+            $show_data = '这是被转成数组的对象:<br>'.print_r($data_array, true);
+        } else {
+            $show_data = print_r($data, true);
+        }
+        $str .= $show_data;
+        $str .= '</pre>';
+        echo $str;
+    }
 }
 
 if ( !function_exists('ajaxReturn') ) {
