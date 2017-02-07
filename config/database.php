d277b0a7b8aa643a9be484a98b0121c0d319ff9a
baijunyao <baijunyao@baijunyao.com>
2017-02-07 22:16:56 +0800

    关闭mysql严格模式

--- config/database.php
+++ config/database.php
@@ -62,7 +62,7 @@ return [
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => 'bjy_',
-            'strict' => true,
+            'strict' => false,
             'engine' => null,
         ],
 
