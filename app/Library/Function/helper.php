82caa0486b9fccd26b2d119c21c75a9593d299c1
baijunyao <baijunyao@baijunyao.com>
2017-02-12 09:24:09 +0800

    距离现在的时间

--- app/Library/Function/helper.php
+++ app/Library/Function/helper.php
@@ -228,3 +228,32 @@ if ( !function_exists('AddTextWater') ) {
         return $image;
     }
 }
+
+if ( !function_exists('wordTime') ) {
+    /**
+     * 把日期或者时间戳转为距离现在的时间
+     *
+     * @param $time
+     * @return bool|string
+     */
+    function wordTime($time) {
+        // 如果是日期格式的时间;则先转为时间戳
+        $int = time() - $time;if (!is_integer($time)) {
+            $time = strtotime($time);
+        }
+        if ($int <= 2){
+            $str = sprintf('刚刚', $int);
+        }elseif ($int < 60){
+            $str = sprintf('%d秒前', $int);
+        }elseif ($int < 3600){
+            $str = sprintf('%d分钟前', floor($int / 60));
+        }elseif ($int < 86400){
+            $str = sprintf('%d小时前', floor($int / 3600));
+        }elseif ($int < 1728000){
+            $str = sprintf('%d天前', floor($int / 86400));
+        }else{
+            $str = date('Y-m-d H:i:s', $time);
+        }
+        return $str;
+    }
+}
