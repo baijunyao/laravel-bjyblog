622f5425f917552fcb3c9335b9b43dc1af89e95b
baijunyao <baijunyao@baijunyao.com>
2017-03-11 21:24:27 +0800

    增加兔斯基表情图片文件

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -102,27 +102,27 @@ class IndexController extends Controller
         // }
 
         // 从旧系统中迁移评论
-        // $data = DB::connection('old')->table('comment')->get()->toArray();
-        // $commentModel->truncate();
-        // foreach ($data as $v) {
-        //     $comment_data = [
-        //         'id' => $v->cmtid,
-        //         'oauth_user_id' => $v->ouid,
-        //         'type' => $v->type,
-        //         'pid' => $v->pid,
-        //         'article_id' => $v->aid,
-        //         'content' => $v->content,
-        //         'status' => $v->status,
-        //     ];
-        //     $commentModel->create($comment_data);
-        //     $editCommentMap = [
-        //         'id' => $v->cmtid,
-        //     ];
-        //     $editCommentData = [
-        //         'created_at' => date('Y-m-d H:i:s', $v->date)
-        //     ];
-        //     $commentModel->editData($editCommentMap, $editCommentData);
-        // }
+        $data = DB::connection('old')->table('comment')->get()->toArray();
+        $commentModel->truncate();
+        foreach ($data as $v) {
+            $comment_data = [
+                'id' => $v->cmtid,
+                'oauth_user_id' => $v->ouid,
+                'type' => $v->type,
+                'pid' => $v->pid,
+                'article_id' => $v->aid,
+                'content' => str_replace('/Public/emote', '/statics/emoticon', $v->content),
+                'status' => $v->status,
+            ];
+            $commentModel->create($comment_data);
+            $editCommentMap = [
+                'id' => $v->cmtid,
+            ];
+            $editCommentData = [
+                'created_at' => date('Y-m-d H:i:s', $v->date)
+            ];
+            $commentModel->editData($editCommentMap, $editCommentData);
+        }
 
         // 迁移友情链接
         // $data = DB::connection('old')->table('link')->get()->toArray();
@@ -153,30 +153,30 @@ class IndexController extends Controller
         // }
 
         // 迁移第三方登录用户表
-        $data = DB::connection('old')->table('oauth_user')->get()->toArray();
-        $oauthUserModel->truncate();
-        foreach ($data as $v) {
-            $oauthUserData = [
-                'id' => $v->id,
-                'type' => $v->type,
-                'name' => $v->nickname,
-                'avatar' => $v->avatar,
-                'openid' => $v->openid,
-                'access_token' => $v->access_token,
-                'last_login_ip' => $v->last_login_ip,
-                'login_times' => $v->login_times,
-                'email' => $v->email,
-                'is_admin' => $v->is_admin
-            ];
-            $oauthUserModel->addData($oauthUserData);
-            $editOauthUserMap = [
-                'id' => $v->id,
-            ];
-            $editOauthUserData = [
-                'created_at' => date('Y-m-d H:i:s', $v->create_time)
-            ];
-            $oauthUserModel->editData($editOauthUserMap, $editOauthUserData);
-        }
+        // $data = DB::connection('old')->table('oauth_user')->get()->toArray();
+        // $oauthUserModel->truncate();
+        // foreach ($data as $v) {
+        //     $oauthUserData = [
+        //         'id' => $v->id,
+        //         'type' => $v->type,
+        //         'name' => $v->nickname,
+        //         'avatar' => $v->head_img,
+        //         'openid' => $v->openid,
+        //         'access_token' => $v->access_token,
+        //         'last_login_ip' => $v->last_login_ip,
+        //         'login_times' => $v->login_times,
+        //         'email' => $v->email,
+        //         'is_admin' => $v->is_admin
+        //     ];
+        //     $oauthUserModel->addData($oauthUserData);
+        //     $editOauthUserMap = [
+        //         'id' => $v->id,
+        //     ];
+        //     $editOauthUserData = [
+        //         'created_at' => date('Y-m-d H:i:s', $v->create_time)
+        //     ];
+        //     $oauthUserModel->editData($editOauthUserMap, $editOauthUserData);
+        // }
 
 
 
