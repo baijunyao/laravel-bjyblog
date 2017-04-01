9cb3739eb09f59dca5078a281ddad0874d1e3724
baijunyao <baijunyao@baijunyao.com>
2017-04-02 01:02:04 +0800

    从旧系统迁移数据先检测是否已锁，迁移完成创建锁文件

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -28,9 +28,23 @@ class IndexController extends Controller
         return view('admin/index/index');
     }
 
-
+    /**
+     * 从thinkphp-bjyblog中迁移数据
+     *
+     * @param Article $articleModel
+     * @param ArticleTag $articleTag
+     * @param Comment $commentModel
+     * @param FriendshipLink $friendshipLinkModel
+     * @param Config $configModel
+     * @param OauthUser $oauthUserModel
+     * @param Chat $chatModel
+     */
     public function migration(Article $articleModel, ArticleTag $articleTag, Comment $commentModel, FriendshipLink $friendshipLinkModel, Config $configModel, OauthUser $oauthUserModel, Chat $chatModel)
     {
+        // 防止误操作清空数据库
+        if (file_exists(storage_path('lock/migration.lock'))) {
+            die('已经迁移过,如需重新迁移,请先删除/storage/lock/migration.lock文件');
+        }
         // 从旧系统中迁移文章
         $htmlConverter = new HtmlConverter();
         $parser = new Parser();
@@ -83,111 +97,113 @@ class IndexController extends Controller
         }
 
         // 从旧系统中迁移文章标签中间表
-        // $data = DB::connection('old')->table('article_tag')->get()->toArray();
-        // $articleTag->truncate();
-        // foreach ($data as $v) {
-        //     $article_tag = [
-        //         'article_id' => $v->aid,
-        //         'tag_id' => $v->tid
-        //     ];
-        //     $articleTag->addData($article_tag);
-        // }
+        $data = DB::connection('old')->table('article_tag')->get()->toArray();
+        $articleTag->truncate();
+        foreach ($data as $v) {
+            $article_tag = [
+                'article_id' => $v->aid,
+                'tag_id' => $v->tid
+            ];
+            $articleTag->addData($article_tag);
+        }
 
         // 从旧系统中迁移评论
-        // $data = DB::connection('old')->table('comment')->get()->toArray();
-        // $commentModel->truncate();
-        // foreach ($data as $v) {
-        //     $comment_data = [
-        //         'id' => $v->cmtid,
-        //         'oauth_user_id' => $v->ouid,
-        //         'type' => $v->type,
-        //         'pid' => $v->pid,
-        //         'article_id' => $v->aid,
-        //         'content' => str_replace('/Public/emote', '/statics/emoticon', $v->content),
-        //         'status' => $v->status,
-        //     ];
-        //     $commentModel->create($comment_data);
-        //     $editCommentMap = [
-        //         'id' => $v->cmtid,
-        //     ];
-        //     $editCommentData = [
-        //         'created_at' => date('Y-m-d H:i:s', $v->date)
-        //     ];
-        //     $commentModel->editData($editCommentMap, $editCommentData);
-        // }
+        $data = DB::connection('old')->table('comment')->get()->toArray();
+        $commentModel->truncate();
+        foreach ($data as $v) {
+            $comment_data = [
+                'id' => $v->cmtid,
+                'oauth_user_id' => $v->ouid,
+                'type' => $v->type,
+                'pid' => $v->pid,
+                'article_id' => $v->aid,
+                'content' => str_replace('/Public/emote', '/statics/emoticon', $v->content),
+                'status' => $v->status,
+            ];
+            $commentModel->create($comment_data);
+            $editCommentMap = [
+                'id' => $v->cmtid,
+            ];
+            $editCommentData = [
+                'created_at' => date('Y-m-d H:i:s', $v->date)
+            ];
+            $commentModel->editData($editCommentMap, $editCommentData);
+        }
 
         // 迁移友情链接
-        // $data = DB::connection('old')->table('link')->get()->toArray();
-        // $friendshipLinkModel->truncate();
-        // foreach ($data as $v) {
-        //     $link_data = [
-        //         'id' => $v->lid,
-        //         'name' => $v->lname,
-        //         'url' => $v->url,
-        //         'sort' => $v->sort
-        //     ];
-        //     if ($v->is_show === 0) {
-        //         $link_data['deleted_at'] = date('Y-m-d H:i:s', time());
-        //     }
-        //     $friendshipLinkModel->addData($link_data);
-        // }
+        $data = DB::connection('old')->table('link')->get()->toArray();
+        $friendshipLinkModel->truncate();
+        foreach ($data as $v) {
+            $link_data = [
+                'id' => $v->lid,
+                'name' => $v->lname,
+                'url' => $v->url,
+                'sort' => $v->sort
+            ];
+            if ($v->is_show === 0) {
+                $link_data['deleted_at'] = date('Y-m-d H:i:s', time());
+            }
+            $friendshipLinkModel->addData($link_data);
+        }
 
         // 迁移配置项
-        // $data = DB::connection('old')->table('config')->get()->toArray();
-        // $configModel->truncate();
-        // foreach ($data as $v) {
-        //     $config_data = [
-        //         'id' => $v->id,
-        //         'name' => $v->name,
-        //         'value' => $v->value
-        //     ];
-        //     $configModel->addData($config_data);
-        // }
+        $data = DB::connection('old')->table('config')->get()->toArray();
+        $configModel->truncate();
+        foreach ($data as $v) {
+            $config_data = [
+                'id' => $v->id,
+                'name' => $v->name,
+                'value' => $v->value
+            ];
+            $configModel->addData($config_data);
+        }
 
         // 迁移第三方登录用户表
-        // $data = DB::connection('old')->table('oauth_user')->get()->toArray();
-        // $oauthUserModel->truncate();
-        // foreach ($data as $v) {
-        //     $oauthUserData = [
-        //         'id' => $v->id,
-        //         'type' => $v->type,
-        //         'name' => $v->nickname,
-        //         'avatar' => $v->head_img,
-        //         'openid' => $v->openid,
-        //         'access_token' => $v->access_token,
-        //         'last_login_ip' => $v->last_login_ip,
-        //         'login_times' => $v->login_times,
-        //         'email' => $v->email,
-        //         'is_admin' => $v->is_admin
-        //     ];
-        //     $oauthUserModel->addData($oauthUserData);
-        //     $editOauthUserMap = [
-        //         'id' => $v->id,
-        //     ];
-        //     $editOauthUserData = [
-        //         'created_at' => date('Y-m-d H:i:s', $v->create_time)
-        //     ];
-        //     $oauthUserModel->editData($editOauthUserMap, $editOauthUserData);
-        // }
+        $data = DB::connection('old')->table('oauth_user')->get()->toArray();
+        $oauthUserModel->truncate();
+        foreach ($data as $v) {
+            $oauthUserData = [
+                'id' => $v->id,
+                'type' => $v->type,
+                'name' => $v->nickname,
+                'avatar' => $v->head_img,
+                'openid' => $v->openid,
+                'access_token' => $v->access_token,
+                'last_login_ip' => $v->last_login_ip,
+                'login_times' => $v->login_times,
+                'email' => $v->email,
+                'is_admin' => $v->is_admin
+            ];
+            $oauthUserModel->addData($oauthUserData);
+            $editOauthUserMap = [
+                'id' => $v->id,
+            ];
+            $editOauthUserData = [
+                'created_at' => date('Y-m-d H:i:s', $v->create_time)
+            ];
+            $oauthUserModel->editData($editOauthUserMap, $editOauthUserData);
+        }
 
         // 迁移随言碎语表
-        // $data = DB::connection('old')->table('chat')->get()->toArray();
-        // $chatModel->truncate();
-        // foreach ($data as $v) {
-        //     $chatData = [
-        //         'id' => $v->chid,
-        //         'content' => $v->content,
-        //     ];
-        //     $chatModel->addData($chatData);
-        //     $editChatMap = [
-        //         'id' => $v->chid,
-        //     ];
-        //     $editChatData = [
-        //         'created_at' => date('Y-m-d H:i:s', $v->date)
-        //     ];
-        //     $chatModel->editData($editChatMap, $editChatData);
-        // }
-
+        $data = DB::connection('old')->table('chat')->get()->toArray();
+        $chatModel->truncate();
+        foreach ($data as $v) {
+            $chatData = [
+                'id' => $v->chid,
+                'content' => $v->content,
+            ];
+            $chatModel->addData($chatData);
+            $editChatMap = [
+                'id' => $v->chid,
+            ];
+            $editChatData = [
+                'created_at' => date('Y-m-d H:i:s', $v->date)
+            ];
+            $chatModel->editData($editChatMap, $editChatData);
+        }
+        echo '数据迁移完成';
+        // 迁移完成创建锁文件
+        file_put_contents(storage_path('lock/migration.lock'), '');
 
     }
 
