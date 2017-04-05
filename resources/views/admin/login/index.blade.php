1a72b545e31619e74b3917486e9635559217baf5
baijunyao <baijunyao@baijunyao.com>
2017-04-05 23:02:54 +0800

    增加404页面

--- resources/views/admin/login/index.blade.php
+++ resources/views/admin/login/index.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>@yield('title') - laravel-bjyadmin</title>
+    <title>管理后台登录 - laravel-bjyblog</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap -->
