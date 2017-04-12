4fbad1c8020c2f1e93ca72e7f2007d4eadacea50
baijunyao <baijunyao@baijunyao.com>
2017-04-12 20:07:19 +0800

    增加stripHtmlTags函数删除html标签

--- app/Library/Function/helper.php
+++ app/Library/Function/helper.php
@@ -287,3 +287,33 @@ if ( !function_exists('markdownToHtml') ) {
 
 
 }
+
+if ( !function_exists('stripHtmlTags') ) {
+
+	/**
+	 * 删除指定标签
+	 *
+	 * @param array $tags     删除的标签  数组形式
+	 * @param string $str     html字符串
+	 * @param bool $content   true保留标签的内容text
+	 * @return mixed
+	 */
+	function stripHtmlTags($tags, $str, $content = true)
+	{
+		$html = [];
+		// 是否保留标签内的text字符
+		if($content){
+			foreach ($tags as $tag) {
+				$html[] = '/(<' . $tag . '.*?>(.|\n)*?<\/' . $tag . '>)/is';
+			}
+		}else{
+			foreach ($tags as $tag) {
+				$html[] = "/(<(?:\/" . $tag . "|" . $tag . ")[^>]*>)/is";
+			}
+		}
+		$data = preg_replace($html, '', $str);
+		return $data;
+	}
+
+
+}
