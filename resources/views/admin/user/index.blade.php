5e8dc82e742926ea96390f34025b3accf9e09400
baijunyao <baijunyao@baijunyao.com>
2017-04-19 23:42:51 +0800

    增加对用户的编辑

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -12,12 +12,14 @@
             <th>用户名</th>
             <th>邮箱</th>
             <th>创建日期</th>
+            <th>编辑</th>
         </tr>
         @foreach($data as $k => $v)
             <tr>
                 <td>{{ $v->name }}</td>
                 <td>{{ $v->email }}</td>
                 <td>{{ $v->created_at }}</td>
+                <td>{{ url('admin/user/edit', [$v->id]) }}</td>
             </tr>
         @endforeach
     </table>
