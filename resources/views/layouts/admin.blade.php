89153ce7c2e6ed1c74df3e26b50af48d5c521f44
baijunyao <baijunyao@baijunyao.com>
2017-04-22 22:42:05 +0800

    删除不用的菜单

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -105,14 +105,6 @@
                                 <span class=" fa fa-angle-down"></span>
                             </a>
                             <ul class="dropdown-menu dropdown-usermenu pull-right">
-                                <li><a href="javascript:;"> Profile</a></li>
-                                <li>
-                                    <a href="javascript:;">
-                                        <span class="badge bg-red pull-right">50%</span>
-                                        <span>Settings</span>
-                                    </a>
-                                </li>
-                                <li><a href="javascript:;">Help</a></li>
                                 <li><a href="{{ url('admin/login/logout') }}"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
                             </ul>
                         </li>
