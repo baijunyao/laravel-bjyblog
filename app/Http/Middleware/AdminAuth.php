f651ebcef8f0cdee15d6cc4420ebc7c17d757e4a
baijunyao <baijunyao@baijunyao.com>
2017-04-25 21:17:11 +0800

    如果没有登录或者不是管理员则重定向到登录页面

--- app/Http/Middleware/AdminAuth.php
+++ app/Http/Middleware/AdminAuth.php
@@ -15,6 +15,10 @@ class AdminAuth
      */
     public function handle($request, Closure $next)
     {
+        // 如果不是管理员或者没有登录;则重定向到登录页面
+        if (session('user.is_admin') !== 1) {
+            return redirect('admin/login/index');
+        }
         return $next($request);
     }
 }
