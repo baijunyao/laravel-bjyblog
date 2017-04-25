30cbc1dc5dfbf653f85ec738b8c98bc1bfe105f6
baijunyao <baijunyao@baijunyao.com>
2017-04-25 21:20:45 +0800

    old后台登录的eamil

--- resources/views/admin/login/index.blade.php
+++ resources/views/admin/login/index.blade.php
@@ -28,7 +28,7 @@
                     {{ csrf_field() }}
                     <h1>管理后台</h1>
                     <div>
-                        <input type="text" class="form-control" placeholder="Email" required="" name="email">
+                        <input type="text" class="form-control" placeholder="Email" required="" name="email" value="{{ old('email') }}">
                     </div>
                     <div>
                         <input type="password" class="form-control" placeholder="Password" required="" name="password">
@@ -63,42 +63,6 @@
                         <div>
                             <h1><i class="fa fa-paw"></i> 白俊遥博客!</h1>
                             <p>©2017 All Rights Reserved. 白俊遥博客! is a Bootstrap 3 template. Privacy and Terms</p>
-                        </div>
-                    </div>
-                </form>
-            </section>
-        </div>
-
-        <div id="register" class="animate form registration_form">
-            <section class="login_content">
-                <form>
-                    <h1>Create Account</h1>
-                    <div>
-                        <input type="text" class="form-control" placeholder="Username" required="" />
-                    </div>
-                    <div>
-                        <input type="email" class="form-control" placeholder="Email" required="" />
-                    </div>
-                    <div>
-                        <input type="password" class="form-control" placeholder="Password" required="" />
-                    </div>
-                    <div>
-                        <a class="btn btn-default submit" href="index.html">Submit</a>
-                    </div>
-
-                    <div class="clearfix"></div>
-
-                    <div class="separator">
-                        <p class="change_link">Already a member ?
-                            <a href="#signin" class="to_register"> Log in </a>
-                        </p>
-
-                        <div class="clearfix"></div>
-                        <br />
-
-                        <div>
-                            <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>
-                            <p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>
                         </div>
                     </div>
                 </form>
