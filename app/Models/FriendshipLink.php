ca04efbe8557d565d6c6dbea669cdc6ab121be32
baijunyao <baijunyao@baijunyao.com>
2017-04-30 07:49:29 +0800

    使用修饰器添加http或者删除/

--- app/Models/FriendshipLink.php
+++ app/Models/FriendshipLink.php
@@ -4,5 +4,20 @@ namespace App\Models;
 
 class FriendshipLink extends Base
 {
-
+    /**
+     * 给url添加http 或者删除/
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function setFirstNameAttribute($value)
+    {
+        // 如果没有http 则补上http
+        if (strpos($value, 'http') === false) {
+            $value = 'http://'.$value;
+        }
+        // 删除右侧的/
+        $value = rtrim($value, '/');
+        $this->attributes['first_name'] = strtolower($value);
+    }
 }
