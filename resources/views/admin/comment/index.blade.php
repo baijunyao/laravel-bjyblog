ac592d978c9076b6f34352bf7ad9a9742d25db69
baijunyao <baijunyao@baijunyao.com>
2017-05-05 21:10:40 +0800

    后台评论列表设置宽度增加删除按钮

--- resources/views/admin/comment/index.blade.php
+++ resources/views/admin/comment/index.blade.php
@@ -9,19 +9,25 @@
 @section('content')
     <table class="table table-striped table-bordered table-hover">
         <tr>
-            <th>评论内容</th>
-            <th>文章</th>
-            <th>用户</th>
-            <th>评论日期</th>
+            <th width="5%">id</th>
+            <th width="40%">评论内容</th>
+            <th width="25%">文章</th>
+            <th width="10%">用户</th>
+            <th width="15%">评论日期</th>
+            <th width="5%">操作</th>
         </tr>
         @foreach($data as $k => $v)
             <tr>
+                <td>{{ $v->id }}</td>
                 <td>{!! htmlspecialchars_decode($v->content) !!}</td>
                 <td>
                     <a href="{{ url('article', ['article' => $v->article_id]) }}" target="_blank">{{ $v->title }}</a>
                 </td>
                 <td>{{ $v->name }}</td>
                 <td>{{ $v->created_at }}</td>
+                <td>
+                    <a href="javascript:if(confirm('确认删除?'))location.href='{{ url('admin/comment/destroy', [$v->id]) }}'">删除</a>
+                </td>
             </tr>
         @endforeach
     </table>
