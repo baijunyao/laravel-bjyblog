6397752b75c4277f00cbeb2bb70b3930b3c3f9ab
baijunyao <baijunyao@baijunyao.com>
2017-05-10 00:13:15 +0800

    开源项目链接新页面打开

--- resources/views/home/index/git.blade.php
+++ resources/views/home/index/git.blade.php
@@ -7,9 +7,9 @@
 @section('description', '白俊遥的php博客,个人技术博客,bjyblog,bjyadmin官方网站')
 
 @section('content')
-    <!-- 左侧开源项目开始 -->
+    {{--左侧开源项目开始--}}
     <div class="col-xs-12 col-md-12 col-lg-8 b-chat">
-        <!-- bjyadmin开始 -->
+        {{--bjyadmin开始--}}
         <script src='http://git.oschina.net/shuaibai123/thinkphp-bjyadmin/widget_preview'></script>
         <style>
             .pro_name a{color: #4183c4;}
@@ -19,9 +19,9 @@
             .osc_git_info{color: #666;}
             .osc_git_main a{color: #4183c4;}
         </style>
-        <!-- bjyadmin结束 -->
+        {{--bjyadmin结束--}}
 
-        <!-- bjyblog开始 -->
+        {{--bjyblog开始--}}
         <script src='http://git.oschina.net/shuaibai123/thinkbjy/widget_preview'></script>
         <style>
             .pro_name a{color: #4183c4;}
@@ -31,9 +31,9 @@
             .osc_git_info{color: #666;}
             .osc_git_main a{color: #4183c4;}
         </style>
-        <!-- bjyblog结束 -->
+        {{--bjyblog结束--}}
 
-        <!-- sublime开始 -->
+        {{--sublime开始--}}
         <script src='http://git.oschina.net/shuaibai123/sublime-thinkphp-bjy/widget_preview'></script>
         <style>
             .pro_name a{color: #4183c4;}
@@ -43,9 +43,9 @@
             .osc_git_info{color: #666;}
             .osc_git_main a{color: #4183c4;}
         </style>
-        <!-- sublime结束 -->
+        {{--sublime结束--}}
 
-        <!-- 资源开始 -->
+        {{--资源开始--}}
         <script src='http://git.oschina.net/shuaibai123/resources/widget_preview'></script>
         <style>
             .pro_name a{color: #4183c4;}
@@ -55,11 +55,20 @@
             .osc_git_info{color: #666;}
             .osc_git_main a{color: #4183c4;}
         </style>
-        <!-- 资源结束 -->
+        {{--资源结束--}}
 
-        <!-- github 上的项目 -->
+        {{--github 上的项目--}}
         <div class="github-widget" data-repo="baijunyao/thinkphp-bjyadmin"></div>
         <div class="github-widget" data-repo="baijunyao/thinkphp-bjyblog"></div>
     </div>
-    <!-- 左侧开源项目结束 -->
+    {{--左侧开源项目结束--}}
+@endsection
+
+@section('js')
+    
+    <script type="text/javascript">
+        $(function(){
+            $('.osc_git_box a,.github-widget a').attr('target','_blank');
+        })
+    </script>
 @endsection
