d8d0bf9f937d5a3d8604bcdf11c538db4610be69
baijunyao <baijunyao@baijunyao.com>
2017-05-18 21:25:31 +0800

    添加标签的验证器完成

---
+++ app/Http/Requests/Tag/Store.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\Tag;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class Store extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'=>'required|unique:categories',
+        ];
+    }
+
+    /**
+     * 定义字段名中文
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'name'=>'分类名',
+        ];
+    }
+}
