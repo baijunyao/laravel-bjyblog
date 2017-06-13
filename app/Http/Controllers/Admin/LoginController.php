16ede8755db99bd580bc8680d04d3082daa53d54
baijunyao <baijunyao@baijunyao.com>
2017-06-13 23:44:05 +0800

    如果有第三方账号管理员；则通过第三方账号登录

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\OauthUser;
 use Auth;
 use App\Http\Controllers\Controller;
 
@@ -12,9 +13,16 @@ class LoginController extends Controller
      *
      * @return mixed
      */
-    public function index()
+    public function index(OauthUser $oauthUserModel)
     {
-        return view('admin/login/index');
+        // 获取是否有第三方用户被设置为管理员
+        $count = $oauthUserModel->where('is_admin', 1)->count();
+        // 如果有第三方账号管理员；则通过第三方账号登录
+        if ($count) {
+            die('请通过第三方账号登录');
+        } else {
+            return view('admin/login/index');
+        }
     }
 
     /**
