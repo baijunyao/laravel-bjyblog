2484c06ce00373e82e57fd611a70c3e916f31635
baijunyao <baijunyao@baijunyao.com>
2017-06-28 22:09:57 +0800

    开源项目增加laravel系列

--- resources/views/home/index/git.blade.php
+++ resources/views/home/index/git.blade.php
@@ -9,7 +9,9 @@
 @section('content')
     {{--左侧开源项目开始--}}
     <div class="col-xs-12 col-md-12 col-lg-8 b-chat">
+
         {{--bjyadmin开始--}}
+        <div class="github-widget" data-repo="baijunyao/thinkphp-bjyadmin"></div>
         <script src='http://git.oschina.net/shuaibai123/thinkphp-bjyadmin/widget_preview'></script>
         <style>
             .pro_name a{color: #4183c4;}
@@ -22,6 +24,7 @@
         {{--bjyadmin结束--}}
 
         {{--bjyblog开始--}}
+        <div class="github-widget" data-repo="baijunyao/thinkphp-bjyblog"></div>
         <script src='http://git.oschina.net/shuaibai123/thinkbjy/widget_preview'></script>
         <style>
             .pro_name a{color: #4183c4;}
@@ -33,8 +36,9 @@
         </style>
         {{--bjyblog结束--}}
 
-        {{--sublime开始--}}
-        <script src='http://git.oschina.net/shuaibai123/sublime-thinkphp-bjy/widget_preview'></script>
+        <div class="github-widget" data-repo="baijunyao/laravel-bjyadmin"></div>
+        <div class="github-widget" data-repo="baijunyao/laravel-bjyblog"></div>
+        <script src='http://git.oschina.net/shuaibai123/laravel-bjyadmin/widget_preview'></script>
         <style>
             .pro_name a{color: #4183c4;}
             .osc_git_title{background-color: #d8e5f1;}
@@ -43,10 +47,7 @@
             .osc_git_info{color: #666;}
             .osc_git_main a{color: #4183c4;}
         </style>
-        {{--sublime结束--}}
-
-        {{--资源开始--}}
-        <script src='http://git.oschina.net/shuaibai123/resources/widget_preview'></script>
+        <script src='http://git.oschina.net/shuaibai123/laravel-bjyblog/widget_preview'></script>
         <style>
             .pro_name a{color: #4183c4;}
             .osc_git_title{background-color: #d8e5f1;}
@@ -55,11 +56,6 @@
             .osc_git_info{color: #666;}
             .osc_git_main a{color: #4183c4;}
         </style>
-        {{--资源结束--}}
-
-        {{--github 上的项目--}}
-        <div class="github-widget" data-repo="baijunyao/thinkphp-bjyadmin"></div>
-        <div class="github-widget" data-repo="baijunyao/thinkphp-bjyblog"></div>
     </div>
     {{--左侧开源项目结束--}}
 @endsection
