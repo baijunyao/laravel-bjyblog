8640eeca82aa8cf57d43fb2a8f484036e7455a38
baijunyao <baijunyao@baijunyao.com>
2017-06-30 23:58:31 +0800

    获取最新评论的方法增加条数限制参数

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -145,18 +145,19 @@ class Comment extends Base
     }
 
     /**
-     * 获取最新评论
+     * 获取指定条数的最新评论
      *
-     * @return mixed
+     * @param int $limit
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
      */
-    public function getNewData()
+    public function getNewData($limit = 20)
     {
         $data = $this->select('comments.content', 'comments.created_at', 'ou.name', 'ou.avatar', 'a.title', 'a.id as article_id')
             ->join('articles as a', 'comments.article_id', 'a.id')
             ->join('oauth_users as ou', 'ou.id', 'comments.oauth_user_id')
             ->orderBy('comments.created_at', 'desc')
             ->where('ou.is_admin', '<>', 1)
-            ->limit(20)
+            ->limit($limit)
             ->get();
         foreach ($data as $k => $v) {
             // 截取文章标题
