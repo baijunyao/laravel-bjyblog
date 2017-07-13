36d16e5db5828cb3d9a5b137f7d320e9f708aae6
baijunyao <baijunyao@baijunyao.com>
2017-07-13 22:59:45 +0800

    删除不需要显示的文字

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -200,7 +200,7 @@
         <!-- footer content -->
         <footer>
             <div class="pull-right">
-                Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+                
             </div>
             <div class="clearfix"></div>
         </footer>
