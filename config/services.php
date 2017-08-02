dba0779f15816d9866b90ddccc9904441a8c8f21
baijunyao <baijunyao@baijunyao.com>
2017-08-02 23:27:07 +0800

    路径动态获取不写死

--- config/services.php
+++ config/services.php
@@ -38,19 +38,19 @@ return [
     'weibo' => [
         'client_id' => env('WEIBO_KEY'),
         'client_secret' => env('WEIBO_SECRET'),
-        'redirect' => env('WEIBO_REDIRECT_URI')
+        'redirect' => env('APP_URL').'/auth/oauth/handleProviderCallback/qq'
     ],
 
     'qq' => [
         'client_id' => env('QQ_KEY'),
         'client_secret' => env('QQ_SECRET'),
-        'redirect' => env('QQ_REDIRECT_URI'),
+        'redirect' => env('APP_URL').'/auth/oauth/handleProviderCallback/qq'
     ],
 
     'github' => [
         'client_id' => env('GITHUB_KEY'),
         'client_secret' => env('GITHUB_SECRET'),
-        'redirect' => env('GITHUB_REDIRECT_URI'),
+        'redirect' => env('APP_URL').'/auth/oauth/handleProviderCallback/qq'
     ],
 
 
