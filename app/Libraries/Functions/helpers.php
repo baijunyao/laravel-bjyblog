bfe5acacc0e9caf8a600807f92aa934f4ad302b7
baijunyao <baijunyao@baijunyao.com>
2017-09-25 20:18:31 +0800

    调整p函数文字说明

--- app/Libraries/Functions/helpers.php
+++ app/Libraries/Functions/helpers.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Auth;
 
 if (!function_exists('p')) {
 	// 传递数据以易于阅读的样式格式化后输出
-	function p($data, $to_array = true)
+	function p($data, $toArray = true)
 	{
 		// 定义样式
 		$str = '<pre style="display: block;padding: 9.5px;margin: 44px 0 0 0;font-size: 13px;line-height: 1.42857;color: #333;word-break: break-all;word-wrap: break-word;background-color: #F5F5F5;border: 1px solid #CCC;border-radius: 4px;">';
@@ -17,14 +17,14 @@ if (!function_exists('p')) {
 		} elseif (is_null($data)) {
 			// 如果是null 直接显示null
 			$show_data = 'null';
-		} elseif (is_object($data) && in_array(get_parent_class($data), ['Illuminate\Support\Collection', 'App\Models\Base']) && $to_array) {
+		} elseif (is_object($data) && in_array(get_parent_class($data), ['Illuminate\Support\Collection', 'App\Models\Base']) && $toArray) {
 			// 把一些集合转成数组形式来查看
 			$data_array = $data->toArray();
-			$show_data = '这是被转成数组的对象:<br>' . print_r($data_array, true);
-		} elseif (is_object($data) && in_array(get_class($data), ['Maatwebsite\Excel\Readers\LaravelExcelReader']) && $to_array) {
+			$show_data = '这是被转成数组的Collection:<br>' . print_r($data_array, true);
+		} elseif (is_object($data) && in_array(get_class($data), ['Maatwebsite\Excel\Readers\LaravelExcelReader']) && $toArray) {
 			// 把一些集合转成数组形式来查看
 			$data_array = $data->toArray();
-			$show_data = '这是被转成数组的对象:<br>' . print_r($data_array, true);
+			$show_data = '这是被转成数组的Collection:<br>' . print_r($data_array, true);
 		} elseif (is_object($data) && in_array(get_class($data), ['Illuminate\Database\Eloquent\Builder'])) {
 			// 直接调用dd 查看
 			dd($data);
