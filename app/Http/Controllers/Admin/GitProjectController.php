c576e203395b1214ac9e14bd40c81be3c5359719
baijunyao <baijunyao@baijunyao.com>
2017-10-21 17:50:24 +0800

    后台开源项目列表页完成

--- app/Http/Controllers/Admin/GitProjectController.php
+++ app/Http/Controllers/Admin/GitProjectController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\GitProject;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -14,7 +15,9 @@ class GitProjectController extends Controller
      */
     public function index()
     {
-        //
+        $data = GitProject::withTrashed()->get();
+        $assign = compact('data');
+        return view('admin.gitProject.index', $assign);
     }
 
     /**
