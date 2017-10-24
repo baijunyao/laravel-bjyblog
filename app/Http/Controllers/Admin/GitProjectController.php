a5e093bcc7aad86e0021187ad64828ca4708293c
baijunyao <baijunyao@baijunyao.com>
2017-10-24 20:22:05 +0800

    删除开源项目功能完成

--- app/Http/Controllers/Admin/GitProjectController.php
+++ app/Http/Controllers/Admin/GitProjectController.php
@@ -108,8 +108,12 @@ class GitProjectController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($id, GitProject $gitProjectModel)
     {
-        //
+        $map = [
+            'id' => $id
+        ];
+        $gitProjectModel->deleteData($map);
+        return redirect()->back();
     }
 }
