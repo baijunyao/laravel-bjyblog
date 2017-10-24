2bdbd688e7b4c9a5d9d799e763aeac30cd2988b9
baijunyao <baijunyao@baijunyao.com>
2017-10-24 22:11:18 +0800

    监听恢复事件

--- app/Observers/CacheClearObserver.php
+++ app/Observers/CacheClearObserver.php
@@ -7,7 +7,7 @@ use Artisan;
 class CacheClearObserver
 {
     /**
-     * 监听创建事件。
+     * 监听创建事件
      */
     public function created()
     {
@@ -15,7 +15,7 @@ class CacheClearObserver
     }
 
     /**
-     * 监听更新事件。
+     * 监听更新事件
      */
     public function updated()
     {
@@ -23,10 +23,16 @@ class CacheClearObserver
     }
 
     /**
-     * 监听删除事件
+     * 监听恢复事件
      */
     public function deleted()
     {
         Artisan::call('cache:clear');
     }
+
+    public function restored()
+    {
+        Artisan::call('cache:clear');
+    }
+
 }
