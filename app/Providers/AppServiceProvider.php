e4f4e1cb8ba80306a5be5633929fc0addeba202c
baijunyao <baijunyao@baijunyao.com>
2017-11-27 10:13:24 +0800

    修复安装时候因为数据库不存在报错的问题

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,9 @@ use File;
 use Cache;
 use App\Observers\CacheClearObserver;
 use Illuminate\Support\ServiceProvider;
+use DB;
+use Illuminate\Database\QueryException;
+use Artisan;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -55,25 +58,31 @@ class AppServiceProvider extends ServiceProvider
             $view->with($assign);
         });
 
-        // 获取配置项
-        $config = Cache::remember('config', 10080, function () {
-            return Config::pluck('value','name');
-        });
+        // 使用 try catch 是为了解决 composer install 时候触发 php artisan optimize 但此时无数据库的问题
+        try {
+            // 获取配置项
+            $config = Cache::remember('config', 10080, function () {
+                return Config::pluck('value','name');
+            });
+            if (empty($config)) {
+                // 用 config 表中的配置项替换 /config/ 目录下文件中的配置项
+                $serviceConfig = [
+                    'services.github.client_id' => $config['GITHUB_CLIENT_ID'],
+                    'services.github.client_secret' => $config['GITHUB_CLIENT_SECRET'],
 
-        // 用 config 表中的配置项替换 /config/ 目录下文件中的配置项
-        $serviceConfig = [
-            'services.github.client_id' => $config['GITHUB_CLIENT_ID'],
-            'services.github.client_secret' => $config['GITHUB_CLIENT_SECRET'],
+                    'services.qq.client_id' => $config['QQ_APP_ID'],
+                    'services.qq.client_secret' => $config['QQ_APP_KEY'],
 
-            'services.qq.client_id' => $config['QQ_APP_ID'],
-            'services.qq.client_secret' => $config['QQ_APP_KEY'],
-
-            'services.weibo.client_id' => $config['SINA_API_KEY'],
-            'services.weibo.client_secret' => $config['SINA_SECRET'],
-        ];
-        config($serviceConfig);
-        // p($serviceConfig);
-
+                    'services.weibo.client_id' => $config['SINA_API_KEY'],
+                    'services.weibo.client_secret' => $config['SINA_SECRET'],
+                ];
+                config($serviceConfig);
+            }
+        } catch (QueryException $e) {
+            // 此处清除缓存是为了解决上面无数据库时缓存时 config 缓存了空数据 db:seed 后 config 走了缓存为空的问题
+            Artisan::call('seeder:clear');
+            $config = [];
+        }
         // 分配全站通用的数据
         view()->composer('*', function ($view) use($config) {
             $assign = [
