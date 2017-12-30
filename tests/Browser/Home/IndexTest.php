67ff8da4c22af1d71d7dcb8113c7bba61076c96c
baijunyao <baijunyao@baijunyao.com>
2017-12-30 15:24:38 +0800

    首页dusk测试完成

--- tests/Browser/Home/IndexTest.php
+++ tests/Browser/Home/IndexTest.php
@@ -1,23 +1,23 @@
 <?php
 
-namespace Tests\Browser;
+namespace Tests\Browser\Home;
 
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-class ExampleTest extends DuskTestCase
+class IndexTest extends DuskTestCase
 {
     /**
-     * A basic browser test example.
+     * A Dusk test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testIndex()
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
-                    ->assertSee('Laravel');
+                    ->assertSee('阅读全文');
         });
     }
 }
