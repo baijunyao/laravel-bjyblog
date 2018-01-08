5dd881db60399d8ddabab95b755725c64fe972ec
baijunyao <baijunyao@baijunyao.com>
2018-01-08 21:26:04 +0800

    把char改为string

--- database/migrations/2017_07_11_225347_create_articles_table.php
+++ database/migrations/2017_07_11_225347_create_articles_table.php
@@ -16,11 +16,11 @@ class CreateArticlesTable extends Migration {
 		{
 			$table->increments('id')->comment('文章表主键');
 			$table->boolean('category_id')->default(0)->comment('分类id');
-			$table->char('title', 100)->default('')->comment('标题');
-			$table->string('author', 15)->default('')->comment('作者');
+			$table->string('title')->default('')->comment('标题');
+			$table->string('author')->default('')->comment('作者');
 			$table->mediumText('markdown')->comment('markdown文章内容');
 			$table->mediumText('html')->comment('markdown转的html页面');
-			$table->char('description')->default('')->comment('描述');
+			$table->string('description')->default('')->comment('描述');
 			$table->string('keywords')->default('')->comment('关键词');
 			$table->string('cover')->default('')->comment('封面图');
 			$table->boolean('is_top')->default(0)->comment('是否置顶 1是 0否');
