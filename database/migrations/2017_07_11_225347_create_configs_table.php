bf541ca6f24189ee8ef5036ffe419311f4482883
Han-MeiM <969437631@qq.com>
2018-01-08 11:01:36 +0800

    删除TEXT的指定长度

--- database/migrations/2017_07_11_225347_create_configs_table.php
+++ database/migrations/2017_07_11_225347_create_configs_table.php
@@ -16,7 +16,7 @@ class CreateConfigsTable extends Migration {
 		{
 			$table->increments('id')->comment('主键');
 			$table->string('name', 100)->default('')->comment('配置项键名');
-			$table->text('value', 65535)->nullable()->comment('配置项键值 1表示开启 0 关闭');
+			$table->text('value')->nullable()->comment('配置项键值 1表示开启 0 关闭');
 			$table->timestamps();
 			$table->softDeletes();
 		});
