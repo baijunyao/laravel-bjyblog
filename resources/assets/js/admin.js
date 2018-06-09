2229f4ab53001d90fb522250e449bfbe430337dd
baijunyao <baijunyao@baijunyao.com>
2018-06-09 21:21:24 +0800

    引入gentelella

---
+++ resources/assets/js/admin.js
@@ -0,0 +1,10 @@
+require('gentelella/vendors/jquery/dist/jquery')
+
+require('gentelella/vendors/bootstrap/dist/js/bootstrap')
+
+require('gentelella/vendors/fastclick/lib/fastclick')
+
+require('gentelella/vendors/nprogress/nprogress')
+
+require('gentelella/build/js/custom')
+
