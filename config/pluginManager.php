8fab8aa37f79a8be722f14c4f51f4d11d02fae6f
baijunyao <baijunyao@baijunyao.com>
2018-07-28 22:05:22 +0800

    使用laravel-jquery

---
+++ config/pluginManager.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'plugin' => [
+        /**
+         * 示例
+         */
+        // [
+        //     'name' => \Baijunyao\LaravelFlash\Manager::class,  // 要使用的插件
+        //     'except' => [
+        //         'home/*' // 排除的路由 此路由下不使用插件
+        //     ]
+        // ],
+    ]
+];
