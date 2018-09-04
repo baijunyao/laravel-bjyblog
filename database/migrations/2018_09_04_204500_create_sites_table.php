a098905667b332fbb51ff63df49fd05725686b08
baijunyao <baijunyao@baijunyao.com>
2018-09-04 22:05:28 +0800

    推荐博客表结构设计完成

---
+++ database/migrations/2018_09_04_204500_create_sites_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sites', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('oauth_user_id')->unsigned()->default(0)->comment('第三方用户id');
+            $table->string('name')->default('')->comment('网站名');
+            $table->string('url')->default('')->comment('网站链接');
+            $table->tinyInteger('audit')->default(0)->comment('审核状态1为通过审核');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sites');
+    }
+}
