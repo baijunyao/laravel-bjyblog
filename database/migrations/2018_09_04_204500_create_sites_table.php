515c211324cc8f780966b12f8762034afcc801b5
baijunyao <baijunyao@baijunyao.com>
2018-09-08 18:00:03 +0800

    site表增加description字段

--- database/migrations/2018_09_04_204500_create_sites_table.php
+++ database/migrations/2018_09_04_204500_create_sites_table.php
@@ -17,6 +17,7 @@ class CreateSitesTable extends Migration
             $table->increments('id');
             $table->integer('oauth_user_id')->unsigned()->default(0)->comment('第三方用户id');
             $table->string('name')->default('')->comment('网站名');
+            $table->string('description')->default('')->comment('描述');
             $table->string('url')->default('')->comment('网站链接');
             $table->tinyInteger('audit')->default(0)->comment('审核状态1为通过审核');
             $table->tinyInteger('sort')->default(0)->comment('排序');
