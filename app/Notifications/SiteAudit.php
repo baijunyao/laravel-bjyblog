71b2d05cfec7e17ed9cf2c2e8c57ac1eedf954a0
baijunyao <baijunyao@baijunyao.com>
2018-09-25 18:33:56 +0800

    只在生产环境发送邮件通知

--- app/Notifications/SiteAudit.php
+++ app/Notifications/SiteAudit.php
@@ -29,7 +29,6 @@ class SiteAudit extends Notification
      */
     public function via($notifiable)
     {
-        return ['mail'];
         // 只在外网生产环境下发送邮件通知
         // if (env('APP_ENV') == 'local') {
         if (env('APP_ENV') == 'production') {
