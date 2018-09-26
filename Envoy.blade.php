837c0bc214259145681ac4f17d125102c1538fa1
baijunyao <baijunyao@baijunyao.com>
2018-09-26 14:22:48 +0800

    Envoy配置完成

---
+++ Envoy.blade.php
@@ -0,0 +1,33 @@
+@setup
+	require __DIR__.'/vendor/autoload.php';
+	$dotenv = new Dotenv\Dotenv(__DIR__);
+	try {
+		$dotenv->load();
+		$dotenv->required(['DEPLOY_IP', 'DEPLOY_PORT', 'DEPLOY_USER', 'DEPLOY_PATH', 'DEPLOY_BRANCH'])->notEmpty();
+	} catch ( Exception $e )  {
+		echo $e->getMessage();
+	}
+
+	$ip = getenv('DEPLOY_IP');
+	$port = getenv('DEPLOY_PORT');
+	$user = getenv('DEPLOY_USER');
+	$path = getenv('DEPLOY_PATH');
+	$branch = getenv('DEPLOY_BRANCH');
+	if ( substr($path, 0, 1) !== '/' ) throw new Exception('path 必须是绝对路径');
+	$path = rtrim($path, '/');
+@endsetup
+
+@servers(['web' => ["$user@$ip -p $port"]])
+
+@task('deploy', ['on' => 'web'])
+	cd {{ $path }}
+	git pull origin {{ $branch }}
+	composer install --no-dev
+	php artisan migrate --force
+	php artisan cache:clear
+	php artisan config:clear
+	php artisan route:clear
+	php artisan view:clear
+	php artisan clear-compiled
+	php artisan queue:restart
+@endtask
