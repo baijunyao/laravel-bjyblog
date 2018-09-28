3f44fa4df4a8156e63e2ae584bdfe943ceec23a7
baijunyao <baijunyao@baijunyao.com>
2018-09-28 16:03:42 +0800

    一键升级多个版本的命令完成

---
+++ app/Console/Commands/Upgrade/AllVersions.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Console\Commands\Upgrade;
+
+use App\Models\Console;
+use Illuminate\Console\Command;
+use File;
+use Artisan;
+
+class AllVersions extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'upgrade:allVersions';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // 运行迁移
+        Artisan::call('migrate', [
+            '--force' => true,
+        ]);
+        $consoleModel = new Console();
+
+        // 获取 Console/Commands/Upgrade 目录下的文件并按创建日期排序
+        $file = collect(File::files(app_path('Console/Commands/Upgrade')))
+            ->transform(function ($v) {
+                return [
+                    'cTime' => $v->getCTime(),
+                    'filename' => basename($v->getFilename(), '.php')
+                ];
+            })
+            ->filter(function ($v) {
+                return $v['filename'] === 'AllVersions' ? false : true;
+            })
+            ->sortBy('cTime')
+            ->pluck('filename');
+
+        // 如果还没有执行过升级命令的则执行升级命令
+        $console = Console::pluck('name');
+        foreach ($file as $k => $v) {
+            $name = 'App\Console\Commands\Upgrade\\' . $v;
+            if ($console->contains($name)) {
+                continue;
+            }
+            $version = strtolower(str_replace('_', '.', $v));
+            $command = 'upgrade:' . $version;
+            Artisan::call($command);
+            $consoleData = [
+                'name' => $name
+            ];
+            $consoleModel->storeData($consoleData);
+            $this->info($version . ' success');
+        }
+    }
+}
---
+++ database/seeds/ConsolesTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ConsolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('consoles')->delete();
+
+        \DB::table('consoles')->insert([
+            [
+                'id' => 1,
+                'name' => 'App\Console\Commands\Upgrade\V5_5_4_1',
+                'created_at' => '2018-09-27 22:26:00',
+                'updated_at' => '2018-09-27 22:26:00',
+                'deleted_at' => NULL,
+            ],
+            [
+                'id' => 2,
+                'name' => 'App\Console\Commands\Upgrade\V5_5_4_3',
+                'created_at' => '2018-09-27 22:26:00',
+                'updated_at' => '2018-09-27 22:26:00',
+                'deleted_at' => NULL,
+            ]
+        ]);
+    }
+}
