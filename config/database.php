abc4584b39e9afd16237fc83e79701ed59b9a89d
baijunyao <baijunyao@baijunyao.com>
2018-11-17 00:13:47 +0800

    完善travis ci

--- config/database.php
+++ config/database.php
@@ -90,7 +90,22 @@ return [
             'prefix' => 'bjy_',
             'strict' => false,
             'engine' => null,
-        ]
+        ],
+
+        'testing' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'test'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => 'bjy_',
+            'strict' => false,
+            'engine' => null,
+        ],
     ],
 
     /*
