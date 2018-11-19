a549a949236d09e5c48be5231a6fdead623b1dd3
baijunyao <baijunyao@baijunyao.com>
2018-11-19 21:39:47 +0800

    自动补全url

--- app/Models/Site.php
+++ app/Models/Site.php
@@ -8,4 +8,15 @@ class Site extends Base
     {
         return $this->belongsTo(OauthUser::class);
     }
+
+    public function setUrlAttribute($value)
+    {
+        // 如果没有http 则补上http
+        if (strpos($value, 'http') === false) {
+            $value = 'http://'.$value;
+        }
+        // 删除右侧的/
+        $value = rtrim($value, '/');
+        $this->attributes['url'] = strtolower($value);
+    }
 }
