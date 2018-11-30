112ea6210e8e6f99f8c5c26557c1c244ae6b0c7d
baijunyao <baijunyao@baijunyao.com>
2018-11-30 19:11:39 +0800

    删除/app/User.php统一使用/app/Models/User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,16 @@
 
 namespace App\Models;
 
-class User extends Base
+use Illuminate\Notifications\Notifiable;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+
+class User extends Base implements AuthenticatableContract, AuthorizableContract
 {
+    use Authenticatable, Authorizable, Notifiable;
     /**
      * The attributes that should be hidden for arrays.
      *
--- config/auth.php
+++ config/auth.php
@@ -77,7 +77,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => App\Models\User::class,
         ],
 
         'oauth' => [
