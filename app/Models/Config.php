bbe60b19eab2814e9a4910b1bfd7212772b75784
baijunyao <baijunyao@baijunyao.com>
2018-12-05 21:41:08 +0800

    增加对值为数组的配置项的支持

--- app/Models/Config.php
+++ app/Models/Config.php
@@ -4,5 +4,24 @@ namespace App\Models;
 
 class Config extends Base
 {
+    /**
+     * 如果 value 是 json 则转成数组
+     *
+     * @param $value
+     * @return mixed
+     */
+    public function getValueAttribute($value)
+    {
+        return is_json($value) ? json_decode($value, true) : $value;
+    }
 
+    /**
+     * 如果 value 是数组 则转成 json
+     *
+     * @param $value
+     */
+    public function setValueAttribute($value)
+    {
+        $this->attributes['value'] = is_array($value) ? json_encode($value) : $value;
+    }
 }
