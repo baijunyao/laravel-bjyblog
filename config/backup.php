380fefff631b983ac787e81116f989226df0fd2e
baijunyao <baijunyao@baijunyao.com>
2018-12-08 17:06:45 +0800

    定义备份存储的目录名

--- config/backup.php
+++ config/backup.php
@@ -8,7 +8,7 @@ return [
          * The name of this application. You can use this name to monitor
          * the backups.
          */
-        'name' => config('app.name'),
+        'name' => 'backup',
 
         'source' => [
 
