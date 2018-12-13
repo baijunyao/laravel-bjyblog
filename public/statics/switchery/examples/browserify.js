e331664807200c2e6a80d248d985ba819c0a35ec
baijunyao <baijunyao@baijunyao.com>
2018-12-13 20:30:41 +0800

    删除重复资源

--- public/statics/switchery/examples/browserify.js
+++
@@ -1,9 +0,0 @@
-switchie = document.createElement('input')
-switchie.id = 'js-switch'
-switchie.type = 'checkbox'
-
-document.body.appendChild(switchie);
-
-var Switchery = require('../switchery');
-var elem = document.querySelector('#js-switch');
-var init = new Switchery(elem);
