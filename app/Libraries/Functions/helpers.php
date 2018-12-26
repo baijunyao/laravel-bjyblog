18f2bb954fee16ac6b5cc307fe909f258ab96f5b
baijunyao <baijunyao@baijunyao.com>
2018-12-26 22:56:25 +0800

    给图片添加水印的时候排除gif

--- app/Libraries/Functions/helpers.php
+++ app/Libraries/Functions/helpers.php
@@ -195,16 +195,19 @@ if ( !function_exists('add_text_water') ) {
      * @return mixed
      */
     function add_text_water($file, $text, $color = '#0B94C1') {
-        $image = Image::make($file);
-        $image->text($text, $image->width()-20, $image->height()-30, function($font) use($color) {
-            $font->file(public_path('fonts/msyh.ttf'));
-            $font->size(15);
-            $font->color($color);
-            $font->align('right');
-            $font->valign('bottom');
-        });
-        $image->save($file);
-        return $image;
+        $extension = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+        dump($extension !== 'gif');die;
+        if ($extension !== 'gif') {
+            $image = Image::make($file);
+            $image->text($text, $image->width()-20, $image->height()-30, function($font) use($color) {
+                $font->file(public_path('fonts/msyh.ttf'));
+                $font->size(15);
+                $font->color($color);
+                $font->align('right');
+                $font->valign('bottom');
+            });
+            $image->save($file);
+        }
     }
 }
 
