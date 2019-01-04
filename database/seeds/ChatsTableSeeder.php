00e8b2b9e3d22061ea20a5adcd2fd3bf98ce6dcf
baijunyao <baijunyao@baijunyao.com>
2019-01-04 18:09:49 +0800

    ChatController测试完成

--- database/seeds/ChatsTableSeeder.php
+++ database/seeds/ChatsTableSeeder.php
@@ -13,12 +13,19 @@ class ChatsTableSeeder extends Seeder
     {
         DB::table('chats')->delete();
         DB::table('chats')->insert([
-            0 =>[
+            [
                 'id' => 1,
                 'content' => '技术这东西；懂的越多；不懂的就越多；',
                 'created_at' => '2017-7-18 07:35:12',
                 'updated_at' => '2016-7-18 07:35:12',
                 'deleted_at' => NULL,
+            ],
+            [
+                'id' => 2,
+                'content' => '已删除',
+                'created_at' => '2019-01-04 16:35:12',
+                'updated_at' => '2019-01-04 16:35:12',
+                'deleted_at' => '2019-01-04 16:35:12',
             ],
         ]);
     }
---
+++ tests/Feature/Admin/ChatControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\Feature\Admin\CURD\TestCreate;
+use Tests\Feature\Admin\CURD\TestDestroy;
+use Tests\Feature\Admin\CURD\TestEdit;
+use Tests\Feature\Admin\CURD\TestForceDelete;
+use Tests\Feature\Admin\CURD\TestIndex;
+use Tests\Feature\Admin\CURD\TestRestore;
+use Tests\Feature\Admin\CURD\TestStore;
+use Tests\Feature\Admin\CURD\TestUpdate;
+
+class ChatControllerTest extends TestCase
+{
+    use TestIndex, TestCreate, TestStore, TestEdit, TestUpdate, TestDestroy, TestRestore, TestForceDelete;
+    protected $urlPrefix = 'admin/chat/';
+    protected $table = 'chats';
+    protected $storeData = [
+        'content' => '新增'
+    ];
+    protected $updateData = [
+        'content' => '编辑'
+    ];
+}
