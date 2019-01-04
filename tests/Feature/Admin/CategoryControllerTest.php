720c75a509f2772b05de863d4b0a486a4278f57c
baijunyao <baijunyao@baijunyao.com>
2019-01-04 18:19:54 +0800

    CategoryController测试完成

---
+++ tests/Feature/Admin/CategoryControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\Feature\Admin\CURD\TestCreate;
+use Tests\Feature\Admin\CURD\TestDestroy;
+use Tests\Feature\Admin\CURD\TestEdit;
+use Tests\Feature\Admin\CURD\TestForceDelete;
+use Tests\Feature\Admin\CURD\TestIndex;
+use Tests\Feature\Admin\CURD\TestRestore;
+use Tests\Feature\Admin\CURD\TestStore;
+use Tests\Feature\Admin\CURD\TestUpdate;
+
+class CategoryControllerTest extends TestCase
+{
+    use TestIndex, TestCreate, TestStore, TestEdit, TestUpdate, TestDestroy, TestRestore, TestForceDelete;
+    protected $urlPrefix = 'admin/category/';
+    protected $table = 'categories';
+    protected $destroyId = 2;
+    protected $restoreId = 3;
+    protected $forceDeleteId = 3;
+    protected $storeData = [
+        'name' => '新增',
+        'keywords' => '新增',
+        'description' => '新增',
+        'sort' => 3,
+    ];
+    protected $updateData = [
+        'name' => '编辑',
+        'keywords' => '编辑',
+        'description' => '编辑',
+        'sort' => 10,
+    ];
+}
