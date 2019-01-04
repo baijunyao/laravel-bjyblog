b48acc431574894ec2a313aad63ce6219906736d
baijunyao <baijunyao@baijunyao.com>
2019-01-04 17:05:19 +0800

    FriendshipLinkController测试完成

--- database/seeds/FriendshipLinksTableSeeder.php
+++ database/seeds/FriendshipLinksTableSeeder.php
@@ -13,7 +13,7 @@ class FriendshipLinksTableSeeder extends Seeder
     {
         DB::table('friendship_links')->delete();
         DB::table('friendship_links')->insert([
-            0 =>[
+            [
                 'id' => 1,
                 'name' => '白俊遥博客',
                 'url' => 'https://baijunyao.com',
@@ -21,6 +21,15 @@ class FriendshipLinksTableSeeder extends Seeder
                 'created_at' => '2017-7-16 07:35:12',
                 'updated_at' => '2016-7-16 07:35:12',
                 'deleted_at' => NULL,
+            ],
+            [
+                'id' => 2,
+                'name' => '已删除',
+                'url' => 'https://deleted.com',
+                'sort' => 2,
+                'created_at' => '2019-01-04 16:35:12',
+                'updated_at' => '2019-01-04 16:35:12',
+                'deleted_at' => '2019-01-04 16:35:12',
             ],
         ]);
     }
---
+++ tests/Feature/Admin/FriendshipLinkControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\Feature\Admin\CURD\TestCreate;
+use Tests\Feature\Admin\CURD\TestDestroy;
+use Tests\Feature\Admin\CURD\TestEdit;
+use Tests\Feature\Admin\CURD\TestForceDelete;
+use Tests\Feature\Admin\CURD\TestIndex;
+use Tests\Feature\Admin\CURD\TestRestore;
+use Tests\Feature\Admin\CURD\TestStore;
+use Tests\Feature\Admin\CURD\TestUpdate;
+
+class FriendshipLinkControllerTest extends TestCase
+{
+    use TestIndex, TestCreate, TestStore, TestEdit, TestUpdate, TestDestroy, TestRestore, TestForceDelete;
+    protected $urlPrefix = 'admin/friendshipLink/';
+    protected $table = 'friendship_links';
+    protected $storeData = [
+        'name' => '新增',
+        'url' => 'https://store.com',
+        'sort' => 3,
+    ];
+    protected $updateData = [
+        'name' => '编辑',
+        'url' => 'https://update.com',
+        'sort' => 3,
+    ];
+}
