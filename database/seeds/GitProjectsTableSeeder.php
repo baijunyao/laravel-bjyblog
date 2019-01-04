fd8b084307dd84943bf9ea08cade2ea177fa877c
baijunyao <baijunyao@baijunyao.com>
2019-01-04 17:01:29 +0800

    GitProjectController测试完成

--- database/seeds/GitProjectsTableSeeder.php
+++ database/seeds/GitProjectsTableSeeder.php
@@ -12,13 +12,9 @@ class GitProjectsTableSeeder extends Seeder
      */
     public function run()
     {
-        
-
-        \DB::table('git_projects')->delete();
-        
-        \DB::table('git_projects')->insert(array (
-            0 => 
-            array (
+        DB::table('git_projects')->delete();
+        DB::table('git_projects')->insert([
+            [
                 'id' => 1,
                 'sort' => 1,
                 'type' => 1,
@@ -26,19 +22,17 @@ class GitProjectsTableSeeder extends Seeder
                 'created_at' => '2017-10-23 21:09:04',
                 'updated_at' => '2017-10-26 21:42:40',
                 'deleted_at' => NULL,
-            ),
-            1 => 
-            array (
+            ],
+            [
                 'id' => 2,
                 'sort' => 2,
                 'type' => 2,
-                'name' => 'shuaibai123/thinkphp-bjyadmin',
+                'name' => 'baijunyao/thinkphp-bjyadmin',
                 'created_at' => '2017-10-26 21:43:07',
                 'updated_at' => '2017-10-26 22:02:28',
                 'deleted_at' => NULL,
-            ),
-            2 => 
-            array (
+            ],
+            [
                 'id' => 3,
                 'sort' => 3,
                 'type' => 1,
@@ -46,19 +40,17 @@ class GitProjectsTableSeeder extends Seeder
                 'created_at' => '2017-10-26 21:43:26',
                 'updated_at' => '2017-10-26 22:02:40',
                 'deleted_at' => NULL,
-            ),
-            3 => 
-            array (
+            ],
+            [
                 'id' => 4,
                 'sort' => 4,
                 'type' => 2,
-                'name' => 'shuaibai123/thinkbjy',
+                'name' => 'baijunyao/thinkbjy',
                 'created_at' => '2017-10-26 21:43:56',
                 'updated_at' => '2017-10-26 22:02:59',
                 'deleted_at' => NULL,
-            ),
-            4 => 
-            array (
+            ],
+            [
                 'id' => 5,
                 'sort' => 5,
                 'type' => 1,
@@ -66,9 +58,8 @@ class GitProjectsTableSeeder extends Seeder
                 'created_at' => '2017-10-26 22:03:15',
                 'updated_at' => '2017-10-26 22:03:15',
                 'deleted_at' => NULL,
-            ),
-            5 => 
-            array (
+            ],
+            [
                 'id' => 6,
                 'sort' => 6,
                 'type' => 1,
@@ -76,28 +67,35 @@ class GitProjectsTableSeeder extends Seeder
                 'created_at' => '2017-10-26 22:03:23',
                 'updated_at' => '2017-10-26 22:03:23',
                 'deleted_at' => NULL,
-            ),
-            6 => 
-            array (
+            ],
+            [
                 'id' => 7,
                 'sort' => 7,
                 'type' => 2,
-                'name' => 'shuaibai123/laravel-bjyadmin',
+                'name' => 'baijunyao/laravel-bjyadmin',
                 'created_at' => '2017-10-26 22:07:24',
                 'updated_at' => '2017-10-26 22:07:59',
                 'deleted_at' => NULL,
-            ),
-            7 => 
-            array (
+            ],
+            [
                 'id' => 8,
                 'sort' => 8,
                 'type' => 2,
-                'name' => 'shuaibai123/laravel-bjyblog',
+                'name' => 'baijunyao/laravel-bjyblog',
                 'created_at' => '2017-10-26 22:07:47',
                 'updated_at' => '2017-10-26 22:08:04',
                 'deleted_at' => NULL,
-            ),
-        ));
+            ],
+            [
+                'id' => 9,
+                'sort' => 9,
+                'type' => 2,
+                'name' => 'deleted/deleted',
+                'created_at' => '2019-01-04 16:35:12',
+                'updated_at' => '2019-01-04 16:35:12',
+                'deleted_at' => '2019-01-04 16:35:12',
+            ],
+        ]);
         
         
     }
---
+++ tests/Feature/Admin/GitProjectControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\Feature\Admin\CURD\TestCreate;
+use Tests\Feature\Admin\CURD\TestDestroy;
+use Tests\Feature\Admin\CURD\TestEdit;
+use Tests\Feature\Admin\CURD\TestForceDelete;
+use Tests\Feature\Admin\CURD\TestIndex;
+use Tests\Feature\Admin\CURD\TestRestore;
+use Tests\Feature\Admin\CURD\TestStore;
+use Tests\Feature\Admin\CURD\TestUpdate;
+
+class GitProjectControllerTest extends TestCase
+{
+    use TestIndex, TestCreate, TestStore, TestEdit, TestUpdate, TestDestroy, TestRestore, TestForceDelete;
+    protected $urlPrefix = 'admin/gitProject/';
+    protected $table = 'git_projects';
+    protected $restoreId = 9;
+    protected $forceDeleteId = 9;
+    protected $storeData = [
+        'sort' => 10,
+        'type' => 1,
+        'name' => 'store/store',
+    ];
+    protected $updateData = [
+        'sort' => 11,
+        'type' => 2,
+        'name' => 'update/update',
+    ];
+
+}
