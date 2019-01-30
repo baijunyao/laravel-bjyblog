882e0bb375184c5c3bd724184337f2f9c2664c21
baijunyao <baijunyao@baijunyao.com>
2019-01-30 21:07:20 +0800

    CategoryObserver extends BaseObserver

--- app/Observers/CategoryObserver.php
+++ app/Observers/CategoryObserver.php
@@ -6,26 +6,8 @@ use App\Models\Article;
 use App\Models\Category;
 use Cache;
 
-class CategoryObserver
+class CategoryObserver extends BaseObserver
 {
-    public function saved()
-    {
-        $this->clearCache();
-    }
-
-    public function created()
-    {
-        flash_success('添加成功');
-    }
-
-    public function updated(Category $category)
-    {
-        // restore() triggering both restored() and updated()
-        if($category->getOriginal('deleted_at') === $category->deleted_at){
-            flash_success('修改成功');
-        }
-    }
-
     public function deleting(Category $category)
     {
         if (Article::where('category_id', $category->id)->count() !== 0) {
@@ -34,25 +16,7 @@ class CategoryObserver
         }
     }
 
-    public function deleted(Category $category)
-    {
-        // delete() and forceDelete() will triggering deleted()
-        if ($category->isForceDeleting()) {
-            flash_success('彻底删除成功');
-        } else {
-            flash_success('删除成功');
-        }
-
-        $this->clearCache();
-    }
-
-    public function restored()
-    {
-        flash_success('恢复成功');
-        $this->clearCache();
-    }
-
-    private function clearCache()
+    protected function clearCache()
     {
         // 更新分类缓存
         Cache::forget('common:category');
