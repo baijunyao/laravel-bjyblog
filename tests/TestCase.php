4f573e394cf622b1d722bf4997ca799a928d6008
baijunyao <baijunyao@baijunyao.com>
2019-02-15 19:26:37 +0800

    Update baijunyao/laravel-test-support to 1.1.*

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,13 @@
 
 namespace Tests;
 
+use App\Providers\ComposerServiceProvider;
 use Baijunyao\LaravelTestSupport\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    protected static $bootstrappers = [
+        ComposerServiceProvider::class
+    ];
 }
