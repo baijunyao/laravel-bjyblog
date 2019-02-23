2cff4d21e007415ceffa4e5084915824e4f2f122
baijunyao <baijunyao@baijunyao.com>
2019-02-23 14:25:53 +0800

    Upgrade to v5.5.10.7

--- config/bjyblog.php
+++ config/bjyblog.php
@@ -2,7 +2,7 @@
 
 return [
     // 博客版本
-    'version' => 'v5.5.10.6',
+    'version' => 'v5.5.10.7',
     'branch' => env('DEPLOY_BRANCH', 'master'),
     /**
      * 以下配置项会在 @see \App\Providers\AppServiceProvider::boot() 中被覆盖
