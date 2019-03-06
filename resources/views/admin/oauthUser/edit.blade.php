225b80f2f0e6f4b524f3545ad14630ecccf7ed01
baijunyao <baijunyao@baijunyao.com>
2019-03-06 22:16:58 +0800

    OAuth user page translate complete

--- resources/views/admin/oauthUser/edit.blade.php
+++ resources/views/admin/oauthUser/edit.blade.php
@@ -1,38 +1,36 @@
 @extends('layouts.admin')
 
-@section('title', '编辑第三方用户')
+@section('title', __('Edit OAuth User'))
 
-@section('nav', '编辑第三方用户')
-
-@section('description', '编辑或者查看第三方用户详情')
+@section('nav', __('Edit OAuth User'))
 
 @section('content')
     <form class="form-inline" action="{{ url('admin/oauthUser/update', [$data->id]) }}" method="post">
         {{ csrf_field() }}
         <table class="table table-striped table-bordered table-hover">
             <tr>
-                <th>类型</th>
+                <th>{{ __('Type') }}</th>
                 <td>
                     @if($data->type == 1)QQ @endif
-                    @if($data->type == 2)新浪微博 @endif
+                    @if($data->type == 2){{ __('Weibo') }} @endif
                     @if($data->type == 3)github @endif
                 </td>
             </tr>
             <tr>
-                <th>邮箱</th>
+                <th>{{ __('Email') }}</th>
                 <td>
                     <input class="form-control" type="text" name="email" value="{{ $data->email }}">
                 </td>
             </tr>
             <tr>
-                <th>用户名</th>
+                <th>{{ __('User Name') }}</th>
                 <td>
                     <input class="form-control" type="text" name="name" value="{{ $data->name }}">
                 </td>
             </tr>
 
             <tr>
-                <th>是否是管理员</th>
+                <th>{{ __('Is Administrator?') }}</th>
                 <td>
                     <input type="checkbox" class="js-switch" name="is_admin" value="1" @if($data->is_admin == 1) checked @endif />
                 </td>
