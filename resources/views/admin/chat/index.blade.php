03c362498f50e67c0f564399b595c211c015e5a6
baijunyao <baijunyao@baijunyao.com>
2019-03-08 21:24:24 +0800

    Chat page translate complete

--- resources/views/admin/chat/index.blade.php
+++ resources/views/admin/chat/index.blade.php
@@ -1,26 +1,24 @@
 @extends('layouts.admin')
 
-@section('title', '随言碎语列表')
+@section('title', __('Chat List'))
 
-@section('nav', '随言碎语列表')
-
-@section('description', '博客随言碎语')
+@section('nav', __('Chat List'))
 
 @section('content')
     <ul id="myTab" class="nav nav-tabs bar_tabs">
         <li class="active">
-            <a href="{{ url('admin/chat/index') }}">随言碎语列表</a>
+            <a href="{{ url('admin/chat/index') }}">{{ __('Chat List') }}</a>
         </li>
         <li>
-            <a href="{{ url('admin/chat/create') }}">添加随言碎语</a>
+            <a href="{{ url('admin/chat/create') }}">{{ __('Add Chat') }}</a>
         </li>
     </ul>
 
     <table class="table table-striped table-bordered table-hover">
         <tr>
             <th width="5%">id</th>
-            <th width="65%">内容</th>
-            <th width="15%">添加日期</th>
+            <th width="65%">{{ __('Content') }}</th>
+            <th width="15%">{{ __('Date') }}</th>
             <th width="5%">{{ __('Status') }}</th>
             <th width="10%">{{ __('Handle') }}</th>
         </tr>
