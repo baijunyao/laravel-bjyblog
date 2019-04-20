8768a39463ef4ea8338a6113801697c7a8fb32fc
baijunyao <baijunyao@baijunyao.com>
2019-04-21 00:15:30 +0800

    Fix send comment mail error

--- app/Observers/CommentObserver.php
+++ app/Observers/CommentObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Jobs\SendCommentEmail;
 use App\Models\Article;
+use App\Models\Comment;
 use App\Models\OauthUser;
 use Cache;
 
@@ -23,6 +24,7 @@ class CommentObserver extends BaseObserver
         // 给站长发送通知邮件
         if ($isAdmin == 0) {
             $emailAddress = config('bjyblog.notification_email');
+
             if (!empty($emailAddress)) {
                 $emailData = [
                     'name'    => '站长',
@@ -31,7 +33,7 @@ class CommentObserver extends BaseObserver
                     'type'    => '评论',
                     'url'     => url('article', [$comment->article_id]) . '#comment-' . $comment->id,
                     'title'   => $title,
-                    'content' => $this->ubbToImage($comment->content),
+                    'content' => $comment->ubbToImage($comment->content),
                 ];
                 $subject = $name . '评论了 ' . $title;
                 dispatch(new SendCommentEmail($emailAddress, '站长', $subject, $emailData));
@@ -40,7 +42,7 @@ class CommentObserver extends BaseObserver
 
         // 给用户发送邮件通知
         if ($comment->pid != 0) {
-            $parent_user_id = self::where('id', $comment->pid)->value('oauth_user_id');
+            $parent_user_id = Comment::where('id', $comment->pid)->value('oauth_user_id');
             $parentData     = OauthUser::select('name', 'email')
                 ->where('id', $parent_user_id)
                 ->first()
@@ -53,7 +55,7 @@ class CommentObserver extends BaseObserver
                     'type'    => '回复',
                     'url'     => url('article', [$comment->article_id]) . '#comment-' . $comment->id,
                     'title'   => $title,
-                    'content' => $this->ubbToImage($comment->content),
+                    'content' => $comment->ubbToImage($comment->content),
                 ];
                 $subject = $name . '评论了 ' . $title;
                 dispatch(new SendCommentEmail($parentData['email'], $parentData['name'], $subject, $emailData));
