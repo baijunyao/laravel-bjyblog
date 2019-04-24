6989426bf357f8328566b5dcb2a41e2a87231bc3
baijunyao <baijunyao@baijunyao.com>
2019-04-24 19:32:22 +0800

    Improve feed

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -323,15 +323,16 @@ class IndexController extends Controller
                 ->latest()
                 ->get();
         });
+
         $feed              = App::make('feed');
-        $feed->title       = '白俊遥';
-        $feed->description = '白俊遥博客';
-        $feed->logo        = 'https://baijunyao.com/uploads/avatar/1.jpg';
+        $feed->title       = config('app.name');
+        $feed->description = config('bjyblog.head.description');
+        $feed->logo        = asset('uploads/avatar/1.jpg');
         $feed->link        = url('feed');
         $feed->setDateFormat('carbon');
-        $feed->pubdate = $article->first()->created_at;
-        $feed->lang    = 'zh-CN';
-        $feed->ctype   = 'application/xml';
+        $feed->pubdate     = $article->first()->created_at;
+        $feed->lang        = config('app.locale');
+        $feed->ctype       = 'application/xml';
 
         foreach ($article as $v) {
             $feed->add($v->title, $v->author, url('article', $v->id), $v->created_at, $v->description);
