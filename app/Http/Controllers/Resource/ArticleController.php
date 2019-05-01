752560541ad8bc27882d8e48e0fe276055352fc1
baijunyao <baijunyao@baijunyao.com>
2019-05-01 09:02:29 +0800

    Article destroy complete

--- app/Http/Controllers/Resource/ArticleController.php
+++ app/Http/Controllers/Resource/ArticleController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers\Resource;
 
+use App\Http\Controllers\Resource\Rest\Destroy;
 use App\Http\Controllers\Resource\Rest\Index;
 use App\Http\Controllers\Resource\Rest\Show;
 use App\Http\Controllers\Resource\Rest\Store;
 use App\Http\Controllers\Resource\Rest\Update;
 use App\Models\Article;
-use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    use Index, Store, Show, Update;
+    use Index, Store, Show, Update, Destroy;
 
     private const MODEL = Article::class;
 
@@ -28,15 +28,4 @@ class ArticleController extends Controller
             'name'
         ],
     ];
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
