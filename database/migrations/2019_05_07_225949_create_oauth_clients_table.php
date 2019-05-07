7d554d19b08c1df5fb53dc2c041f4b864e02477e
baijunyao <baijunyao@baijunyao.com>
2019-05-07 23:30:19 +0800

    Create oauth_clients table

---
+++ database/migrations/2019_05_07_225949_create_oauth_clients_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOauthClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('oauth_clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->default('');
+            $table->string('client_id')->default('');
+            $table->string('client_secret')->default('');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('oauth_clients');
+    }
+}
