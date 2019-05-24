743db6ebad8dd21e3947d8a224e2c830ac994311
baijunyao <baijunyao@baijunyao.com>
2019-05-25 02:38:54 +0800

    Upgrade to v5.8.4.0

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.8.17 on 2019-05-19 17:27:22.
+ * Generated for Laravel 5.8.18 on 2019-05-25 02:31:02.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
@@ -5867,6 +5867,7 @@ namespace Illuminate\Support\Facades {
          *
          * @param object|string $class
          * @return mixed 
+         * @throws \Illuminate\Contracts\Container\BindingResolutionException
          * @static 
          */ 
         public static function resolvePolicy($class)
