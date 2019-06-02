5aea58ff288c437cff413c471449336e1828ab73
baijunyao <baijunyao@baijunyao.com>
2019-06-02 21:06:40 +0800

    Use blog version as sentry release

--- config/sentry.php
+++ config/sentry.php
@@ -3,8 +3,7 @@
 return array(
     'dsn' => env('SENTRY_LARAVEL_DSN'),
 
-    // capture release as git sha
-    // 'release' => trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD')),
+    'release' => config('bjyblog.version'),
 
     'breadcrumbs' => [
 
