8a8de16efa850b2ced5d436e4f40f00f81c51260
baijunyao <baijunyao@baijunyao.com>
2019-06-05 23:07:12 +0800

    Add social share default config

--- database/seeds/ConfigsTableSeeder.php
+++ database/seeds/ConfigsTableSeeder.php
@@ -321,7 +321,12 @@ class ConfigsTableSeeder extends Seeder
             [
                 'id'         => 169,
                 'name'       => 'bjyblog.social_share.jssocials_config',
-                'value'      => '{}',
+                'value'      => '{
+    shares: ["email", "twitter", "facebook", "googleplus", "linkedin", "pinterest", "stumbleupon", "pocket", "whatsapp", "messenger", "vkontakte", "telegram", "line"],
+    showLabel: false,
+    showCount: false,
+    shareIn: "popup"
+}',
                 'created_at' => '2019-05-27 22:22:00',
                 'updated_at' => '2019-05-27 22:22:00',
                 'deleted_at' => null,
@@ -329,7 +334,9 @@ class ConfigsTableSeeder extends Seeder
             [
                 'id'         => 170,
                 'name'       => 'bjyblog.social_share.sharejs_config',
-                'value'      => '{}',
+                'value'      => '{
+    sites: ["weibo", "qq", "wechat", "douban", "qzone", "linkedin", "facebook", "twitter", "google"]
+}',
                 'created_at' => '2019-05-27 22:22:00',
                 'updated_at' => '2019-05-27 22:22:00',
                 'deleted_at' => null,
