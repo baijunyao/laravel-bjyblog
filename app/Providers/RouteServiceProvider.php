e8ea3ef90b48b3e8e787f3658ad5baf1c6cd8b7e
baijunyao <baijunyao@baijunyao.com>
2019-06-16 21:07:26 +0800

    Add redirect route file

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -52,6 +52,10 @@ class RouteServiceProvider extends ServiceProvider
         Route::middleware('web')
             ->namespace($this->namespace)
             ->group(base_path('routes/web.php'));
+
+        Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/redirect.php'));
     }
 
     /**
---
+++ routes/redirect.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('auth/oauth/redirectToProvider/{service}', 'RedirectController@authOauthRedirectToProvider');
+Route::get('auth/oauth/handleProviderCallback/{service}', 'RedirectController@handleProviderCallback');
+Route::get('auth/oauth/logout', 'RedirectController@logout');
