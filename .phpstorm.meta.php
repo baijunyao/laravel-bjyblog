2d97364bb411bccafdf132f5c9aa9a701f35ec7c
baijunyao <baijunyao@baijunyao.com>
2019-06-23 00:01:04 +0800

    Upgrade to v5.8.6.0

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-06-15 13:30:22.
+    * Generated on 2019-06-22 23:59:04.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
