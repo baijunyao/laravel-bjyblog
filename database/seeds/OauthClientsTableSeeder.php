f1d26de2efd44d78e055ac371104e6679d9ef37b
baijunyao <baijunyao@baijunyao.com>
2019-07-23 19:26:27 +0800

    Improve auth test

--- database/seeds/OauthClientsTableSeeder.php
+++ database/seeds/OauthClientsTableSeeder.php
@@ -14,16 +14,16 @@ class OauthClientsTableSeeder extends Seeder
         DB::table('oauth_clients')->truncate();
         DB::table('oauth_clients')->insert([
             [
-                'id'                => 1,
+                'id' => 1,
                 'user_id' => null,
-                'name'              => config('app.name').' Password Grant Client',
-                'secret'       => Str::random(40),
-                'redirect'               => 'http://localhost',
-                'personal_access_client'             => 0,
-                'password_client'              => 1,
-                'revoked'              => 0,
-                'created_at'        => '2019-06-29 20:35:12',
-                'updated_at'        => '2019-06-29 20:35:12',
+                'name' => config('app.name').' Password Grant Client',
+                'secret' => '',
+                'redirect' => 'http://localhost',
+                'personal_access_client' => 0,
+                'password_client' => 1,
+                'revoked' => 0,
+                'created_at' => '2019-06-29 20:35:12',
+                'updated_at' => '2019-06-29 20:35:12',
             ]
         ]);
     }
