6aeb7e6189d2443e10daac3ba28db040b737a926
baijunyao <baijunyao@baijunyao.com>
2019-08-01 21:46:41 +0800

    Add Russian translate file

---
+++ resources/lang/ru/others.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'search_article' => 'Searched for articles about <span class="b-highlight">:word</span>',
+    'article_with_tag' => 'Article with <span class="b-highlight">:tag</span> tag',
+    'comment_count' => 'Total <span class="b-highlight">:count</span> comments',
+];
