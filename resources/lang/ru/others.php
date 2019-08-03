28c4d383ac0d50fed9e49c732256385814c5a554
Yuran <slaed2@yandex.ru>
2019-08-03 17:11:58 +0300

    Update others.php

--- resources/lang/ru/others.php
+++ resources/lang/ru/others.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'search_article' => 'Searched for articles about <span class="b-highlight">:word</span>',
-    'article_with_tag' => 'Article with <span class="b-highlight">:tag</span> tag',
-    'comment_count' => 'Total <span class="b-highlight">:count</span> comments',
+    'search_article' => 'Найдено статей со словом <span class="b-highlight">:word</span>',
+    'article_with_tag' => 'Статьи с тегом <span class="b-highlight">:tag</span> tag',
+    'comment_count' => 'Всего <span class="b-highlight">:count</span> comments',
 ];
