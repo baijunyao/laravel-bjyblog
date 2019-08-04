5fb6efe93a901efcef71587e8f31b7b22a42dc4b
baijunyao <baijunyao@baijunyao.com>
2019-08-04 09:24:37 +0800

    Improve Russian translate resolved #73

--- resources/lang/ru/others.php
+++ resources/lang/ru/others.php
@@ -2,6 +2,6 @@
 
 return [
     'search_article' => 'Найдено статей со словом <span class="b-highlight">:word</span>',
-    'article_with_tag' => 'Статьи с тегом <span class="b-highlight">:tag</span> tag',
-    'comment_count' => 'Всего <span class="b-highlight">:count</span> comments',
+    'article_with_tag' => 'Статьи с тегом <span class="b-highlight">:tag</span> теги',
+    'comment_count' => 'Всего <span class="b-highlight">:count</span> комментарии',
 ];
