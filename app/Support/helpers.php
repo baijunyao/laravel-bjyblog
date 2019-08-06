f3d2ec0b559d3c7526e908fd0f90125bc5e85fb9
baijunyao <baijunyao@baijunyao.com>
2019-08-06 20:03:11 +0800

    Add cdn_url function

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
@@ -337,5 +338,18 @@ if (!function_exists('generate_english_slug')) {
         }
 
         return Str::slug($content);
+    }
+}
+
+if (!function_exists('cdn_url')) {
+    /**
+     * Generate a url for the CDN.
+     *
+     * @param $path
+     * @return string
+     */
+    function cdn_url($path)
+    {
+        return URL::assetFrom(config('bjyblog.cdn_domain'), $path);
     }
 }
