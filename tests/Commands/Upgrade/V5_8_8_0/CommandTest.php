0a4874085a03309c06c19407c8f204c455550252
baijunyao <baijunyao@baijunyao.com>
2019-08-16 21:03:25 +0800

    Add test for upgrade v5.8.8.0 command

---
+++ tests/Commands/Upgrade/V5_8_8_0/CommandTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Commands\Upgrade\V5_8_8_0;
+
+use Artisan;
+use DB;
+
+class CommandTest extends \Tests\Commands\Upgrade\TestCase
+{
+    public function testCommand()
+    {
+        Artisan::call('upgrade:v5.8.8.0');
+
+        $this->assertDatabaseHas('socialite_clients', [
+            'id'            => 6,
+            'name'          => 'vkontakte',
+            'icon'          => 'vk',
+            'client_id'     => '',
+            'client_secret' => '',
+            'created_at'    => '2019-07-01 23:26:38',
+            'updated_at'    => '2019-07-01 23:26:38',
+            'deleted_at'    => null,
+        ]);
+    }
+}
