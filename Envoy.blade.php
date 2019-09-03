7e50358997d38c0ed84a76d28649783d09e9a792
baijunyao <baijunyao@baijunyao.com>
2019-09-03 23:56:55 +0800

    Replace tabs with spaces

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,27 +1,27 @@
 @setup
-	require __DIR__.'/vendor/autoload.php';
-	$dotenv = Dotenv\Dotenv::create(__DIR__);
-	try {
-		$dotenv->load();
-		$dotenv->required(['DEPLOY_IP', 'DEPLOY_PORT', 'DEPLOY_USER', 'DEPLOY_PATH', 'DEPLOY_BRANCH'])->notEmpty();
-	} catch ( Exception $e )  {
-		echo $e->getMessage();
-	}
+    require __DIR__.'/vendor/autoload.php';
+    $dotenv = Dotenv\Dotenv::create(__DIR__);
+    try {
+        $dotenv->load();
+        $dotenv->required(['DEPLOY_IP', 'DEPLOY_PORT', 'DEPLOY_USER', 'DEPLOY_PATH', 'DEPLOY_BRANCH'])->notEmpty();
+    } catch ( Exception $e )  {
+        echo $e->getMessage();
+    }
 
-	$ip = getenv('DEPLOY_IP');
-	$port = getenv('DEPLOY_PORT');
-	$user = getenv('DEPLOY_USER');
-	$path = getenv('DEPLOY_PATH');
-	$branch = getenv('DEPLOY_BRANCH');
-	if ( substr($path, 0, 1) !== '/' ) throw new Exception('Must be an absolute path');
-	$path = rtrim($path, '/');
+    $ip = getenv('DEPLOY_IP');
+    $port = getenv('DEPLOY_PORT');
+    $user = getenv('DEPLOY_USER');
+    $path = getenv('DEPLOY_PATH');
+    $branch = getenv('DEPLOY_BRANCH');
+    if ( substr($path, 0, 1) !== '/' ) throw new Exception('Must be an absolute path');
+    $path = rtrim($path, '/');
 @endsetup
 
 @servers(['web' => ["$user@$ip -p $port"]])
 
 @task('deploy', ['on' => 'web'])
-	cd {{ $path }}
-	git pull origin {{ $branch }}
+    cd {{ $path }}
+    git pull origin {{ $branch }}
     composer install --no-dev --optimize-autoloader
-	php artisan bjyblog:update
+    php artisan bjyblog:update
 @endtask
