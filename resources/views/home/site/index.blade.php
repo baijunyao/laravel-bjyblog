4a4e208e37ccd9248596969c43d5733ce7d4e69e
baijunyao <baijunyao@baijunyao.com>
2019-09-11 22:21:47 +0800

    Improve `Site` translation (#84)

--- resources/views/home/site/index.blade.php
+++ resources/views/home/site/index.blade.php
@@ -35,34 +35,34 @@
                 <div class="col-xs-12 col-md-12 col-lg-12">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h4 class="modal-title b-ta-center" id="myModalLabel">申请 <span class="b-hint">(请确保已添加本站的友情链接)</span></h4>
+                        <h4 class="modal-title b-ta-center" id="myModalLabel">{{ __('Apply') }} <span class="b-hint">({{ __('Please add a friendly link to this site') }})</span></h4>
                     </div>
                 </div>
                 <div class="col-xs-12 col-md-12 col-lg-12 b-submit-site">
                     <form class="form-horizontal" role="form" action="{{ url('site/store') }}" method="post">
                         {{ csrf_field() }}
                         <div class="form-group">
-                            <label for="name" class="col-sm-2 control-label">名称</label>
+                            <label for="name" class="col-sm-2 control-label">{{ __('Site Name') }}</label>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control" name="name" placeholder="请输入网站名">
+                                <input type="text" class="form-control" name="name">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="url" class="col-sm-2 control-label">链接</label>
+                            <label for="url" class="col-sm-2 control-label">{{ __('URL') }}</label>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control" name="url" placeholder="请输入URL">
+                                <input type="text" class="form-control" name="url">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="description" class="col-sm-2 control-label">描述</label>
+                            <label for="description" class="col-sm-2 control-label">{{ __('Description') }}</label>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control" name="description" placeholder="请输入博客描述">
+                                <input type="text" class="form-control" name="description">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="email" class="col-sm-2 control-label">邮箱</label>
+                            <label for="email" class="col-sm-2 control-label">{{ __('Email') }}</label>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control" name="email" placeholder="请输入邮箱用于接收通知">
+                                <input type="text" class="form-control" name="email" placeholder="{{ __('Notification Email') }}">
                             </div>
                         </div>
 
@@ -70,7 +70,7 @@
                 </div>
                 <div class="col-xs-12 col-md-12 col-lg-12">
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-primary center-block b-s-submit">提交</button>
+                        <button type="button" class="btn btn-primary center-block b-s-submit">{{ __('Submit') }}</button>
                     </div>
                 </div>
             </div>
