666fcade327495c9d8323dac7aa05f8cf93b241e
baijunyao <baijunyao@baijunyao.com>
2019-10-16 23:40:48 +0800

    Add `upgrade:v6.2.0` command

---
+++ tests/Commands/Upgrade/V6_2_0/CommandTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Commands\Upgrade\V6_2_0;
+
+use Artisan;
+
+class CommandTest extends \Tests\Commands\Upgrade\TestCase
+{
+    public function testCommand()
+    {
+        Artisan::call('upgrade:v6.2.0');
+
+        static::assertFalse(
+            strpos(
+                file_get_contents(base_path('.env')),
+                'QUEUE_DRIVER'
+            )
+        );
+
+        $this->assertDatabaseHasColumns('tag', ['keywords', 'description']);
+    }
+}
