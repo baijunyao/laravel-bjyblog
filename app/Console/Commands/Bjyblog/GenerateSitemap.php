c7f05f375029244693a356a7ac177e87a40a24ed
baijunyao <baijunyao@baijunyao.com>
2019-10-27 19:06:28 +0800

    Remove spatie/laravel-sitemap

--- app/Console/Commands/Bjyblog/GenerateSitemap.php
+++ app/Console/Commands/Bjyblog/GenerateSitemap.php
@@ -2,9 +2,13 @@
 
 namespace App\Console\Commands\Bjyblog;
 
+use App\Models\Article;
+use App\Models\Category;
+use App\Models\Nav;
+use App\Models\Tag;
+use DateTime;
+use File;
 use Illuminate\Console\Command;
-use Psr\Http\Message\UriInterface;
-use Spatie\Sitemap\SitemapGenerator;
 
 class GenerateSitemap extends Command
 {
@@ -39,15 +43,68 @@ class GenerateSitemap extends Command
      */
     public function handle()
     {
-        $this->info('Start generating the sitemap.xml file');
+        $articles = Article::select('id', 'updated_at')
+            ->latest('updated_at')
+            ->get();
 
-        SitemapGenerator::create(config('app.url'))
-            ->shouldCrawl(function (UriInterface $url) {
-                return strpos($url->getPath(), '/auth/oauth/redirectToProvider') === false;
-            })
-            ->writeToFile(public_path('sitemap.xml'));
+        $categories = Category::select('id', 'updated_at')
+            ->latest('updated_at')
+            ->get();
+
+        $tags = Tag::select('id', 'updated_at')
+            ->latest('updated_at')
+            ->get();
+
+        $navs = Nav::select('id', 'updated_at')
+            ->latest('updated_at')
+            ->get();
+
+        $url = '';
+        foreach ($articles as $article) {
+            /** @var \App\Models\Article $article */
+            $url .= $this->generateUrl(url('article', $article->id), $article->updated_at->format(DateTime::ATOM));
+        }
+
+        foreach ($categories as $category) {
+            /** @var \App\Models\Category $category */
+            $url .= $this->generateUrl(url('category', $category->id), $category->updated_at->format(DateTime::ATOM));
+        }
+
+        foreach ($tags as $tag) {
+            /** @var \App\Models\Tag $tag */
+            $url .= $this->generateUrl(url('tag', $tag->id), $tag->updated_at->format(DateTime::ATOM));
+        }
+
+        foreach ($navs as $nav) {
+            /** @var \App\Models\Nav $nav */
+            $url .= $this->generateUrl(url('nav', $nav->id), $nav->updated_at->format(DateTime::ATOM));
+        }
+
+        $url = rtrim($url);
+
+        $xml = <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+$url
+</urlset>
+XML;
+
+        File::put(public_path('sitemap.xml'), $xml);
 
         $this->info('Generating the sitemap completed.');
         $this->info('Path: ' . public_path('sitemap.xml'));
+    }
+
+    public function generateUrl($loc, $lastmod)
+    {
+        return <<<XML
+    <url>
+        <loc>$loc</loc>
+        <lastmod>$lastmod</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+    </url>
+
+XML;
     }
 }
