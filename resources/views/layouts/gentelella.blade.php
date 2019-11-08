6873536b57d39ae7acba1c5dcf5635b63fedc2de
baijunyao <baijunyao@baijunyao.com>
2019-11-08 20:12:37 +0800

    Improve translation

--- resources/views/layouts/gentelella.blade.php
+++ resources/views/layouts/gentelella.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>@yield('title') - 管理后台</title>
+    <title>@yield('title') - {{ __('Admin panel') }}</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
