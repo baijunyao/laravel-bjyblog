ff462c2ceb702c656c19d28da013e662bad06f39
baijunyao <baijunyao@baijunyao.com>
2019-11-29 23:16:17 +0800

    Rename `ApplySite` to `SiteApply`

--- app/Notifications/SiteApply.php
+++ app/Notifications/SiteApply.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class ApplySite extends Notification implements ShouldQueue
+class SiteApply extends Notification implements ShouldQueue
 {
     use Queueable;
 
