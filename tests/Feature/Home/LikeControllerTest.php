a7926ddc59113ff4a1a851f6311308b37925d36f
baijunyao <baijunyao@baijunyao.com>
2019-12-13 20:17:51 +0800

    Add Article like test

---
+++ tests/Feature/Home/LikeControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\Home;
+
+use App\Models\Article;
+use Tests\Feature\Admin\CURD\TestStore;
+
+class LikeControllerTest extends TestCase
+{
+    protected $urlPrefix = 'like/';
+    protected $storeData = [
+        'article_id' => 1,
+    ];
+
+    public function testStore()
+    {
+        $this->UserPost('store', $this->storeData)
+            ->assertStatus(200);
+
+        static::assertTrue(auth()->guard('socialite')->user()->hasLiked(Article::find(1)));
+    }
+
+    public function testDestroy()
+    {
+        $this->UserDelete('destroy', $this->storeData)
+            ->assertStatus(201);
+
+        static::assertFalse(auth()->guard('socialite')->user()->hasLiked(Article::find(1)));
+    }
+}
