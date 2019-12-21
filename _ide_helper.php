9819544e3f71b43f0417331beb92bdb5e6a0a0a5
baijunyao <baijunyao@baijunyao.com>
2019-12-21 11:42:30 +0800

    Bump version to v6.7.0

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.6.2 on 2019-12-07 12:14:11.
+ * Generated for Laravel 6.9.0 on 2019-12-21 11:19:38.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
@@ -10300,8 +10300,8 @@ namespace Illuminate\Support\Facades {
          * Retrieve an input item from the request.
          *
          * @param string|null $key
-         * @param string|array|null $default
-         * @return string|array|null 
+         * @param mixed $default
+         * @return mixed 
          * @static 
          */ 
         public static function input($key = null, $default = null)
