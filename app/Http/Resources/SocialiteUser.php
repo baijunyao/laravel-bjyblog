9eaa975ac0d980a529140f1bc2ffd1daaf98f5c3
baijunyao <baijunyao@baijunyao.com>
2020-01-05 11:49:33 +0800

    SocialiteUser resource class completed

--- app/Http/Resources/SocialiteUser.php
+++ app/Http/Resources/SocialiteUser.php
@@ -2,6 +2,27 @@
 
 namespace App\Http\Resources;
 
+/**
+ * @mixin \App\Models\SocialiteUser
+ */
 class SocialiteUser extends Base
 {
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name'=> $this->name,
+            'email' => $this->email,
+            'avatar' => $this->avatar,
+            'is_admin'=> $this->is_admin,
+            'socialite_client' => [
+                'id'   => $this->socialiteClient->id,
+                'name' => $this->socialiteClient->name,
+                'icon' => $this->socialiteClient->icon,
+            ],
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => $this->deleted_at,
+        ];
+    }
 }
