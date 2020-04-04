52fc5cdfa11de78a1d30f90cbdf5a4352f456d0c
baijunyao <baijunyao@baijunyao.com>
2020-04-04 14:42:52 +0800

    Bump version to v8.0.0

--- config/bjyblog.php
+++ config/bjyblog.php
@@ -2,7 +2,7 @@
 
 return [
     // 博客版本
-    'version' => 'v7.0.2',
+    'version' => 'v8.0.0',
     'branch' => env('DEPLOY_BRANCH', 'master'),
     /**
      * 以下配置项会在 @see \App\Providers\ComposerServiceProvider::boot() 中被覆盖
