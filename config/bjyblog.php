12ae064b410028284e2c97c29c69a7a2bf5efc2b
baijunyao <baijunyao@baijunyao.com>
2020-06-20 23:59:07 +0800

    Bump version to v11.0.0

--- config/bjyblog.php
+++ config/bjyblog.php
@@ -2,7 +2,7 @@
 
 return [
     // 博客版本
-    'version' => 'v10.0.4',
+    'version' => 'v11.0.0',
     'branch' => env('DEPLOY_BRANCH', 'master'),
     /**
      * 以下配置项会在 @see \App\Providers\ComposerServiceProvider::boot() 中被覆盖
