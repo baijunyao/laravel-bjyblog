0ed0d259ce6dbbebe845e5f4bdeaa4c6c1af7ab5
baijunyao <baijunyao@baijunyao.com>
2020-08-07 23:24:29 +0800

    Improve database seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -8,26 +8,17 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-        $file = collect(File::files(database_path('seeds')))
-            ->transform(function ($v) {
-                return [
-                    'cTime'    => $v->getCTime(),
-                    'filename' => basename($v->getFilename(), '.php'),
-                ];
-            })
-            ->filter(function ($v) {
-                return $v['filename'] === 'DatabaseSeeder' ? false : true;
-            })
-            ->sortBy('cTime')
-            ->pluck('filename');
-        // 自动 call 目录下的文件
-        foreach ($file as $k => $v) {
-            $this->call($v);
+        $files = File::files(database_path('seeds'));
+
+        foreach ($files as $file) {
+            $className = $file->getFilenameWithoutExtension();
+
+            if ($className !== __CLASS__) {
+                $this->call($className);
+            }
         }
     }
 }
