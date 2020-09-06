30ec11ff343bbf228dcf279c88013b2a5ef5bf8d
baijunyao <baijunyao@baijunyao.com>
2020-09-06 15:17:07 +0800

    Revert "Merge branch 'feature/github-theme' into develop"

--- app/Http/Controllers/Home/ArticleController.php
+++ app/Http/Controllers/Home/ArticleController.php
@@ -9,103 +9,34 @@ use App\Http\Controllers\Controller;
 use App\Models\Article;
 use App\Models\Comment;
 use Cache;
-use Carbon\Carbon;
-use Carbon\CarbonPeriod;
 use Illuminate\Http\Request;
-use RuntimeException;
 use Str;
 
 class ArticleController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $head = config('bjyblog.head');
+        $articles = Article::select(
+            'id', 'category_id', 'title',
+            'slug', 'author', 'description',
+            'cover', 'is_top', 'created_at'
+        )
+            ->orderBy('created_at', 'desc')
+            ->with(['category', 'tags'])
+            ->paginate(10);
 
-        if (config('bjyblog.theme') === 'blueberry') {
-            $articles = Article::select(
-                'id', 'category_id', 'title',
-                'slug', 'author', 'description',
-                'cover', 'is_top', 'created_at'
-            )
-                ->orderBy('created_at', 'desc')
-                ->with(['category', 'tags'])
-                ->paginate(10);
+        $head = [
+            'title'       => config('bjyblog.head.title'),
+            'keywords'    => config('bjyblog.head.keywords'),
+            'description' => config('bjyblog.head.description'),
+        ];
 
-            $assign = [
-                'category_id'  => 'index',
-                'articles'     => $articles,
-                'head'         => $head,
-                'tagName'      => '',
-            ];
-        } elseif (config('bjyblog.theme') === 'github') {
-            $pinnedArticles = Article::where('is_top', 1)
-                ->with('category', 'tags')
-                ->orderBy('created_at', 'desc')
-                ->get();
-
-            $selectedYear = Carbon::createFromFormat('Y', $request->input('year', now()->year));
-
-            $yearArticles     = Article::whereYear('created_at', '=', $selectedYear)->get();
-            $firstArticleDate = Article::orderBy('created_at')->value('created_at') ?? now();
-
-            $calendarGraphYears = [];
-
-            while (!$firstArticleDate->isCurrentYear()) {
-                $calendarGraphYears[] = $firstArticleDate->year;
-
-                $firstArticleDate = $firstArticleDate->addYear();
-            }
-
-            $calendarGraphYears[] = now()->year;
-
-            rsort($calendarGraphYears);
-
-            $articlesCount = Article::selectRaw('COUNT(*) AS count, DATE(created_at) as date')
-                ->groupBy('date')
-                ->get()
-                ->keyBy('date')
-                ->toArray();
-
-            $allDaysOfTheYear = CarbonPeriod::create($selectedYear->firstOfYear()->format('Y-m-d'), $selectedYear->endOfYear()->format('Y-m-d'));
-
-            $calendarGraph = [];
-            $x             = 14;
-
-            foreach ($allDaysOfTheYear as $key => $date) {
-                /** @var \Carbon\Carbon $date */
-                $dateString = $date->format('Y-m-d');
-                $count      = $articlesCount[$dateString]['count'] ?? 0;
-                $fill       = $count === 0 ? '#ebedf0' : '#c6e48b';
-
-                if ($date->month === 12 && $date->weekOfYear === 1) {
-                    $calendarGraph[53][] = [
-                        'x'     => $x,
-                        'y'     => $date->dayOfWeek * 13,
-                        'date'  => $dateString,
-                        'count' => $count,
-                        'fill'  => $fill,
-                    ];
-                } else {
-                    $weekOfYear = $date->dayOfWeek === 0 ? $date->weekOfYear + 1 : $date->weekOfYear;
-
-                    $calendarGraph[$weekOfYear][] = [
-                        'x'     => $x,
-                        'y'     => $date->dayOfWeek * 13,
-                        'date'  => $dateString,
-                        'count' => $count,
-                        'fill'  => $fill,
-                    ];
-                }
-
-                if ($date->dayOfWeekIso === 7) {
-                    $x--;
-                }
-            }
-
-            $assign = compact('yearArticles', 'pinnedArticles', 'calendarGraphYears', 'calendarGraph', 'head', 'selectedYear');
-        } else {
-            throw new RuntimeException('Unsupported theme.');
-        }
+        $assign = [
+            'category_id'  => 'index',
+            'articles'     => $articles,
+            'head'         => $head,
+            'tagName'      => '',
+        ];
 
         return view('home.index.index', $assign);
     }
