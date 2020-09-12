398b67df4409d482a595e3d2cc64a5b16bc31fea
baijunyao <baijunyao@baijunyao.com>
2020-09-12 12:21:52 +0800

    Bump version to v11.1.0

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -1999,7 +1999,7 @@ namespace PHPSTORM_META {
             'view.engine.resolver' => \Illuminate\View\Engines\EngineResolver::class,
             'view.finder' => \Illuminate\View\FileViewFinder::class,
         ]));
-    override(\Psr\Container\ContainerInterface::get, map([
+    override(\Psr\Container\ContainerInterface::get(0), map([
         '' => '@',
             'Asm89\Stack\CorsService' => \Asm89\Stack\CorsService::class,
             'DaveJamesMiller\Breadcrumbs\BreadcrumbsGenerator' => \DaveJamesMiller\Breadcrumbs\BreadcrumbsGenerator::class,
