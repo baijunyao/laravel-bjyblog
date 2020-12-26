686729871e29318b4c27d3fc05fddd9148ca1825
baijunyao <baijunyao@baijunyao.com>
2020-12-26 08:44:42 +0800

    Bump version to v14.1.9

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 8.19.0.
+ * Generated for Laravel 8.20.1.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
@@ -7338,7 +7338,7 @@
                     /**
          * Queue a new e-mail message for sending.
          *
-         * @param string|array $view
+         * @param \Illuminate\Contracts\Mail\Mailable|string|array $view
          * @param string|null $queue
          * @return mixed 
          * @static 
