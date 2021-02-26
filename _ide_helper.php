334bba6ce864bb9153bcfa6ba184719562cbb945
baijunyao <baijunyao@baijunyao.com>
2021-02-27 06:52:01 +0800

    Bump version to v15.0.1

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 8.28.1.
+ * Generated for Laravel 8.29.0.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
@@ -8162,7 +8162,7 @@
                     /**
          * Register a callback to be executed when creating job payloads.
          *
-         * @param callable $callback
+         * @param callable|null $callback
          * @return void 
          * @static 
          */ 
