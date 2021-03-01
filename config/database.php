4a121e1203bb35dd325aa441f6824c3e06e81e8e
baijunyao <baijunyao@baijunyao.com>
2021-03-01 21:54:06 +0800

    Update baijunyao/laravel-test-support to v5

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => config('app.env') === 'testing' ? \Baijunyao\LaravelTestSupport\Database::createRandomDatabase() : env('DB_DATABASE', 'forge'),
+            'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
