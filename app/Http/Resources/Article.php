71bff4536fa9bfacc9268976d172e7fcaf536833
baijunyao <baijunyao@baijunyao.com>
2021-05-17 22:36:09 +0800

    Add `@mixin` to resource class

--- app/Http/Resources/Article.php
+++ app/Http/Resources/Article.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Http\Resources;
 
+/**
+ * @mixin \App\Models\Article
+ */
 class Article extends Base
 {
     /**
--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Http\Resources;
 
+/**
+ * @mixin \App\Models\User
+ */
 class User extends Base
 {
 }
