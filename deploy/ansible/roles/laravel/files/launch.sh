1e676d8492422b9c2a9c96a6eba7cd69126b7f29
baijunyao <baijunyao@baijunyao.com>
2021-09-12 23:57:51 +0800

    Add memory and cpu for multipass

--- deploy/ansible/roles/laravel/files/launch.sh
+++ deploy/ansible/roles/laravel/files/launch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-multipass launch --name k3s --mem 4G --disk 20G --cpus 2
+multipass launch --name k3s --mem 8G --disk 20G --cpus 6
 multipass mount $HOME/Sites/laravel-bjyblog k3s:/laravel-bjyblog/site
